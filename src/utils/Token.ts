141573a0406d54102a0603ec4d982121fff9506c
Kerman Kohli <kermankohli@MacBook-Pro.hub>
2020-07-26 08:37:02 +1000

    Change wallet to signer in Token.ts

--- src/utils/Token.ts
+++ src/utils/Token.ts
@@ -1,11 +1,11 @@
 import { BigNumberish } from 'ethers/utils';
 import { BaseERC20 } from '../typings/BaseERC20';
-import { Wallet } from 'ethers';
+import { Signer } from 'ethers';
 
 export default class Token {
   constructor() {}
 
-  static async approve(token: string, owner: Wallet, to: string, value: BigNumberish) {
+  static async approve(token: string, owner: Signer, to: string, value: BigNumberish) {
     const contract = BaseERC20.at(owner, token);
     return await contract.approve(to, value);
   }
@@ -15,13 +15,13 @@ export default class Token {
     from: string,
     to: string,
     value: BigNumberish,
-    caller: Wallet,
+    caller: Signer,
   ) {
     const contract = BaseERC20.at(caller, token);
     return await contract.transferFrom(from, to, value);
   }
 
-  static async transfer(token: string, to: string, value: BigNumberish, caller: Wallet) {
+  static async transfer(token: string, to: string, value: BigNumberish, caller: Signer) {
     const contract = BaseERC20.at(caller, token);
     return await contract.transfer(to, value);
   }
