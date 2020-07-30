a0f50a15f758d92f24698ad94ab5cc7927b719d9
Kerman Kohli <kermankohli@MacBook-Pro.hub>
2020-07-30 16:53:42 +1000

    Updated subgraph + addresses

--- subgraph/mapping.ts
+++ subgraph/mapping.ts
@@ -5,23 +5,25 @@ import {
   GlobalParamsUpdated,
   TotalParUpdated,
 } from '../generated/StateV1/StateV1';
+import { log } from '@graphprotocol/graph-ts';
 
 export function actionOperated(event: ActionOperated): void {
   if (
-    event.params.updatedPosition.owner.toString() == '0x0000000000000000000000000000000000000000'
+    !(
+      event.params.updatedPosition.borrowedAsset == 0 &&
+      event.params.updatedPosition.collateralAsset == 0
+    )
   ) {
-    return;
+    let position = new Position(event.params.params.id.toHex());
+    position.owner = event.params.updatedPosition.owner;
+    position.collateralAsset = event.params.updatedPosition.collateralAsset;
+    position.borrowedAsset = event.params.updatedPosition.borrowedAsset;
+    position.collateralAmountSign = event.params.updatedPosition.collateralAmount.sign;
+    position.collateralAmountValue = event.params.updatedPosition.collateralAmount.value;
+    position.borrowedAmountSign = event.params.updatedPosition.borrowedAmount.sign;
+    position.borrowedAmountValue = event.params.updatedPosition.borrowedAmount.value;
+    position.save();
   }
-
-  let position = new Position(event.params.params.id.toHex());
-  position.owner = event.params.updatedPosition.owner;
-  position.collateralAsset = event.params.updatedPosition.collateralAsset;
-  position.borrowedAsset = event.params.updatedPosition.borrowedAsset;
-  position.collateralAmountSign = event.params.updatedPosition.collateralAmount.sign;
-  position.collateralAmountValue = event.params.updatedPosition.collateralAmount.value;
-  position.borrowedAmountSign = event.params.updatedPosition.borrowedAmount.sign;
-  position.borrowedAmountValue = event.params.updatedPosition.borrowedAmount.value;
-  position.save();
 }
 
 export function logIndexUpdated(event: LogIndexUpdated): void {
