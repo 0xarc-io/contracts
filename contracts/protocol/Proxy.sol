c435154b8c225db6b20a0885a9d1b9a3ddc6fbfa
Kerman Kohli <kermankohli@gmail.com>
2020-08-09 11:08:15 +1000

    Change proxy implementation to unstructured storage

--- contracts/protocol/Proxy.sol
+++ contracts/protocol/Proxy.sol
@@ -3,141 +3,37 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
-import {AdminStorage} from "./Storage.sol";
+/* solium-disable-next-line */
+import { AdminUpgradeabilityProxy } from "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
 
-contract Proxy is AdminStorage {
-
-    constructor() public {
-        // Set admin to caller
-        admin = msg.sender;
-    }
-
-    /*** Admin Functions ***/
-    function setPendingImplementation(
-        address newPendingImplementation
+contract Proxy is
+    AdminUpgradeabilityProxy
+{
+    /**
+     * @dev The constructor of the proxy that sets the admin and logic.
+     *
+     * @param  logic  The address of the contract that implements the underlying logic.
+     * @param  admin  The address of the admin of the proxy.
+     * @param  data   Any data to send immediately to the implementation contract.
+     */
+    constructor(
+        address logic,
+        address admin,
+        bytes memory data
     )
         public
-        returns (uint)
-    {
-
-        require(
-            msg.sender == admin,
-            "Proxy.setPendingImplementation(): can only be set by admin"
-        );
-
-        address oldPendingImplementation = pendingCoreImplementation;
-
-        pendingCoreImplementation = newPendingImplementation;
-
-        emit NewPendingImplementation(
-            oldPendingImplementation,
-            pendingCoreImplementation
-        );
-    }
+        AdminUpgradeabilityProxy(
+            logic,
+            admin,
+            data
+        )
+    {}
 
     /**
-    * @notice Accepts new implementation of comptroller. msg.sender must be pendingImplementation
-    * @dev Admin function for new implementation to accept it's role as implementation
-    * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
-    */
-    function acceptImplementation() public returns (uint) {
-
-        // Check caller is pendingImplementation and pendingImplementation ≠ address(0)
-        require(
-            msg.sender == pendingCoreImplementation || pendingCoreImplementation != address(0),
-            "Proxy.acceptImplementation: must set a valid implementation"
-        );
-
-        // Save current values for inclusion in log
-        address oldImplementation = coreImplementation;
-        address oldPendingImplementation = pendingCoreImplementation;
-
-        coreImplementation = pendingCoreImplementation;
-
-        pendingCoreImplementation = address(0);
-
-        emit NewImplementation(
-            oldImplementation,
-            coreImplementation
-        );
-
-        emit NewPendingImplementation(
-            oldPendingImplementation,
-            pendingCoreImplementation
-        );
-    }
-
-
-    /**
-      * @notice Begins transfer of admin rights. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
-      * @dev Admin function to begin change of admin. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
-      * @param newPendingAdmin New pending admin.
-      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
-      */
-    function setPendingAdmin(address newPendingAdmin) public returns (uint) {
-
-        // Check caller = admin
-        require(
-            msg.sender == admin,
-            "Proxy.setPendingAdmin(): can only be set by the admin"
-        );
-
-        // Save current value, if any, for inclusion in log
-        address oldPendingAdmin = pendingAdmin;
-
-        // Store pendingAdmin with value newPendingAdmin
-        pendingAdmin = newPendingAdmin;
-
-        // Emit NewPendingAdmin(oldPendingAdmin, newPendingAdmin)
-        emit NewPendingAdmin(
-            oldPendingAdmin,
-            newPendingAdmin
-        );
-    }
-
-    /**
-      * @notice Accepts transfer of admin rights. msg.sender must be pendingAdmin
-      * @dev Admin function for pending admin to accept role and update admin
-      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
-      */
-    function acceptAdmin() public returns (uint) {
-
-        // Check caller is pendingAdmin and pendingAdmin ≠ address(0)
-        require(
-            msg.sender == pendingAdmin || msg.sender != address(0),
-            "Proxy.acceptAdmin(): must be accepted by the new admin"
-        );
-
-        // Save current values for inclusion in log
-        address oldAdmin = admin;
-        address oldPendingAdmin = pendingAdmin;
-
-        // Store admin with value pendingAdmin
-        admin = pendingAdmin;
-
-        // Clear the pending value
-        pendingAdmin = address(0);
-
-        emit NewAdmin(oldAdmin, admin);
-        emit NewPendingAdmin(oldPendingAdmin, pendingAdmin);
-    }
-
-    /**
-     * @dev Delegates execution to an implementation contract.
-     * It returns to the external caller whatever the implementation returns
-     * or forwards reverts.
+     * @dev Overrides the default functionality that prevents the admin from reaching the
+     *  implementation contract.
      */
-    function () payable external {
-        // delegate all other functions to current implementation
-        (bool success, ) = coreImplementation.delegatecall(msg.data);
-
-        assembly {
-              let free_mem_ptr := mload(0x40)
-              returndatacopy(free_mem_ptr, 0, returndatasize)
-
-              switch success
-              case 0 { revert(free_mem_ptr, returndatasize) }
-              default { return(free_mem_ptr, returndatasize) }
-        }
-    }
+    function _willFallback()
+        internal
+    { /* solium-disable-line no-empty-blocks */ }
 }
