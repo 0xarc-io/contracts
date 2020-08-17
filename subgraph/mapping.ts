49e2e3f7ff42a237f38408dde7510bf2432167c3
Kerman Kohli <kermankohli@gmail.com>
2020-08-17 22:17:37 +1000

    Update graph events

--- subgraph/mapping.ts
+++ subgraph/mapping.ts
@@ -1,5 +1,5 @@
-import { Position, GlobalParam } from '../generated/schema';
-import { GlobalParamsUpdated } from '../generated/StateV1/StateV1';
+import { Position, GlobalMarket, GlobalRisk } from '../generated/schema';
+import { RiskParamsUpdated, MarketParamsUpdated } from '../generated/StateV1/StateV1';
 import { log } from '@graphprotocol/graph-ts';
 import { ActionOperated } from '../generated/CoreV1/CoreV1';
 
@@ -10,7 +10,13 @@ export function actionOperated(event: ActionOperated): void {
       event.params.updatedPosition.collateralAsset == 0
     )
   ) {
-    let position = new Position(event.params.params.id.toHex());
+    let position = Position.load(event.params.params.id.toHex());
+
+    if (position == null) {
+      position = new Position(event.params.params.id.toHex());
+      position.createdAt = event.block.timestamp;
+    }
+
     position.owner = event.params.updatedPosition.owner;
     position.collateralAsset = event.params.updatedPosition.collateralAsset;
     position.borrowedAsset = event.params.updatedPosition.borrowedAsset;
@@ -22,14 +28,18 @@ export function actionOperated(event: ActionOperated): void {
   }
 }
 
-export function globalParamsUpdated(event: GlobalParamsUpdated): void {
-  let globalParams = new GlobalParam(event.block.timestamp.toString());
-  globalParams.collateralAsset = event.params.updatedParams.collateralAsset;
-  globalParams.syntheticAsset = event.params.updatedParams.syntheticAsset;
-  globalParams.collateralRatio = event.params.updatedParams.collateralRatio.value;
-  globalParams.syntheticRatio = event.params.updatedParams.syntheticRatio.value;
-  globalParams.liquidationSpread = event.params.updatedParams.liquidationSpread.value;
-  globalParams.originationFee = event.params.updatedParams.originationFee.value;
-  globalParams.oracle = event.params.updatedParams.oracle;
+export function marketParamsUpdated(event: MarketParamsUpdated): void {
+  let globalParams = new GlobalMarket(event.block.timestamp.toString());
+  globalParams.collateralRatio = event.params.updatedMarket.collateralRatio.value;
+  globalParams.liquidationArcFee = event.params.updatedMarket.liquidationArcFee.value;
+  globalParams.liquidationUserFee = event.params.updatedMarket.liquidationUserFee.value;
   globalParams.save();
+}
+
+export function riskParamsUpdated(event: RiskParamsUpdated): void {
+  let riskParams = new GlobalRisk(event.block.timestamp.toString());
+  riskParams.collateralLimit = event.params.updatedParams.collateralLimit;
+  riskParams.syntheticLimit = event.params.updatedParams.syntheticLimit;
+  riskParams.positionCollateralMinimum = event.params.updatedParams.positionCollateralMinimum;
+  riskParams.save();
 }
