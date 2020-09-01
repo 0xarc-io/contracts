a8f09536ed6505503f755d0aaa7f3648d05da788
Kerman Kohli <kermankohli@gmail.com>
2020-09-01 14:51:33 +1000

    QSP-11

--- contracts/v1/CoreV1.sol
+++ contracts/v1/CoreV1.sol
@@ -239,7 +239,10 @@ contract CoreV1 is StorageV1, Adminable {
             borrowedAmount: TypesV1.zeroPar()
         });
 
-        // SUGGESTION: Talk about why this was done
+        // This position is saved to storage to make the logic around borrowing
+        // uniform. This is slightly gas inefficient but ok given the ability to
+        // ensure no diverging logic.
+
         uint256 positionId = state.savePosition(newPosition);
 
         newPosition = borrow(
