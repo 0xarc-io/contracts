1ad4c18a5af21636fa99088a45316e1623388cf4
Kerman Kohli <kermankohli@gmail.com>
2020-10-05 21:23:15 +1100

    Update deployment config

--- publish/src/util.ts
+++ publish/src/util.ts
@@ -175,8 +175,6 @@ const performTransactionalStep = async ({
     ? await contract.functions.admin()
     : await contract.functions.getAdmin();
 
-  console.log(await contract.functions.getAdmin());
-
   if (owner === account.address || owner == ethers.constants.AddressZero) {
     // perform action
     let hash;
