84fa302ea6b08815af5218177e0e6fe54da5cbd7
Kerman Kohli <kermankohli@gmail.com>
2020-10-29 15:21:46 +1100

    feature: Finished making the watcher script!

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -1,30 +1,47 @@
 const chokidar = require('chokidar');
-const jest = require('jest');
+const _ = require('lodash');
 const system = require('system-commands');
+const shell = require('shelljs'); // This module is already a solidity-coverage dep
+
+const { gray, green, yellow, redBright, red } = require('chalk');
+
+let lastRunPath = process.argv[process.argv.length - 1];
+runTest(lastRunPath);
 
 // ============ Watchers ============ //
 
-jest.run();
+chokidar.watch('./contracts').on(
+	'change',
+	_.debounce(async (path, event) => {
+		console.log(yellow(`Contracts changed: ${path}`));
+		system('yarn compile')
+			.then((data) => {
+				return system('yarn generate-typings');
+			})
+			.then((data) => {
+				return runTest(lastRunPath);
+			})
+			.catch(console.error);
+	}),
+	500
+);
 
-chokidar.watch('./contracts').on('change', async (event, path) => {
-  system('yarn compile')
-    .then((data) => {
-      console.log(data);
-      return system('yarn generate-typings');
-    })
-    .then((data) => {
-      console.log(data);
-      return jest.run();
-    })
-    .catch(console.error);
-});
+chokidar.watch('./test').on(
+	'change',
+	_.debounce(async (path, event) => {
+		console.log(yellow(`Tests changed: ${path}`));
+		await runTest(path);
+	}),
+	500
+);
 
-chokidar.watch('./test').on('change', async (event, path) => {
-  jest.run();
-});
+async function runTest(path) {
+	if (!path) {
+		return;
+	}
 
-// chokidar.watch('./contracts').on('unlink', (event, path) => {
-//   system('echo Cleaning build...');
-//   exesystemcute('yarn clean');
-//   system('yarn build');
-// });
+	lastRunPath = path;
+	shell.exec(`yarn buidler test ${path}`);
+	let splitPath = lastRunPath.split('/');
+	console.log(green(`Finished running: ${splitPath[splitPath.length - 1]}`));
+}
