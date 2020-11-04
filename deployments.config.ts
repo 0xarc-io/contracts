503e862990f12ee106485a0089a0b8e05135310c
Kerman Kohli <kermankohli@gmail.com>
2020-11-04 20:35:40 +1100

    feature: basic deployment system working!

---
+++ deployments.config.ts
@@ -0,0 +1,39 @@
+import { red } from 'chalk';
+import path, { PlatformPath } from 'path';
+
+// load the data in explicitly (not programmatically) so webpack knows what to bundle
+export const networkPaths = {
+  local: {
+    deployment: require('./deployments/local/deployed.json'),
+    synthConfig: require('./deployments/local/synth-config.json'),
+  },
+  rinkeby: {
+    deployment: require('./deployments/rinkeby/deployed.json'),
+    synthConfig: require('./deployments/rinkeby/synth-config.json'),
+  },
+  mainnet: {
+    deployment: require('./deployments/mainnet/deployed.json'),
+    synthConfig: require('./deployments/mainnet/synth-config.json'),
+  },
+};
+
+export const networks = ['local', 'rinkeby', 'mainnet'];
+
+export const constants = {
+  BUILD_FOLDER: 'build',
+  CONTRACTS_FOLDER: 'contracts',
+  SYNTH_CONFIG_FILENAME: 'synth-config.json',
+  DEPLOYMENT_FILENAME: 'deployed.json',
+};
+
+export function getPathToNetwork(
+  network: string = 'mainnet',
+  file: string = '',
+  platformPath: PlatformPath,
+) {
+  if (!networks.includes(network)) {
+    throw red(`** ${network} is not a valid network **`);
+  }
+
+  return path.join(__dirname, 'deployments', network, file);
+}
---
+++ deployments/src/deployContract.ts
@@ -0,0 +1,70 @@
+import { ethers, Signer } from 'ethers';
+import { writeToDeployments, DeploymentType } from './writeToDeployments';
+import { blue, yellow, gray, green, red } from 'chalk';
+import { loadContracts } from './loadContracts';
+
+export interface DeployContractParams {
+  name: string;
+  source: string;
+  data: ethers.utils.UnsignedTransaction;
+  version: number;
+  type: DeploymentType;
+  group?: string;
+}
+
+export interface NetworkParams {
+  signer: Signer;
+  network: string;
+  gasPrice?: string;
+  gasLimit?: string;
+}
+
+export async function deployContract(
+  deployParams: DeployContractParams,
+  networkParams: NetworkParams,
+) {
+  // Ignore source since it may vary based on the network
+  const existing = loadContracts({
+    network: networkParams.network,
+    name: deployParams.name,
+    type: deployParams.type,
+    group: deployParams.group,
+  });
+
+  const tx = deployParams.data;
+
+  tx.gasLimit = networkParams.gasLimit || deployParams.data.gasLimit;
+  tx.gasPrice = networkParams.gasPrice || deployParams.data.gasPrice;
+
+  const details = `${deployParams.name} | ${deployParams.source} | ${deployParams.group}`;
+
+  if (existing.length) {
+    console.log(red(`Contract already exists: ${details}`));
+    return existing[0].address;
+  }
+
+  console.log(yellow(`* Deploying: ${details}`));
+  const signedTx = await networkParams.signer.sendTransaction(tx);
+
+  console.log(gray(`* Sending tx: ${signedTx.hash}`));
+
+  try {
+    const receipt = await signedTx.wait();
+    console.log(green(`Deployed: ${details}\n`));
+
+    await writeToDeployments({
+      name: deployParams.name,
+      source: deployParams.source,
+      address: receipt.contractAddress,
+      txn: signedTx.hash,
+      network: networkParams.network,
+      version: 1,
+      type: deployParams.type,
+      group: deployParams.group,
+    });
+
+    return receipt.contractAddress;
+  } catch {
+    console.log(red(`Failed to deploy: ${details}\n`));
+  }
+}
