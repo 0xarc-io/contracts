c6e8cbd6edf74a081595a5f557bba4546d9c2223
Kerman Kohli <kermankohli@gmail.com>
2020-11-06 07:29:02 +1100

    fix: error with test constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigNumber } from 'ethers/utils';
 
-export let TEN_PERCENT = BigInt.fromI32(3022265980);
-export let FIVE_PERCENT = BigInt.fromI32(1547125957);
+export let TEN_PERCENT = new BigNumber(3022265980);
+export let FIVE_PERCENT = new BigNumber(1547125957);
 export let ONE_YEAR_IN_SECONDS = 31536000;
-export let BASE = BigInt.fromI32(10).pow(18);
+export let BASE = new BigNumber(10).pow(18);
