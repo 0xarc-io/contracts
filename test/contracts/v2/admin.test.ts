20926cc329d31d3b36fc527c07f4389e499f41ba
Kerman Kohli <kermankohli@gmail.com>
2020-11-06 07:11:16 +1100

    test: D2Core.admin done

--- test/contracts/v2/admin.test.ts
+++ test/contracts/v2/admin.test.ts
@@ -1,36 +1,101 @@
-// import 'module-alias/register';
+import 'module-alias/register';
 
-// import ArcNumber from '@src/utils/ArcNumber';
-// import ArcDecimal from '@src/utils/ArcDecimal';
-// import { Account, getWaffleExpect } from '../../helpers/testingUtils';
-// import d2ArcDescribe, { initializeD2Arc } from '@test/helpers/d2ArcDescribe';
-// import { ITestContext } from '@test/helpers/d2ArcDescribe';
-// import { D2ArcOptions } from '../../helpers/d2ArcDescribe';
+import ArcDecimal from '@src/utils/ArcDecimal';
+import {
+  Account,
+  addSnapshotBeforeRestoreAfterEach,
+  getWaffleExpect,
+} from '../../helpers/testingUtils';
+import { BigNumber } from 'ethers/utils';
+import { ADMINABLE_ERROR, INTEREST_SETTER_ERROR } from '../../helpers/contractErrors';
+import { getAccounts } from '../../helpers/testingUtils';
+import { MockD2CoreV1 } from '@src/typings';
+import { Signer } from 'ethers';
+import { D2TestArc } from '../../../src/D2TestArc';
+import { d2Setup, initializeD2Arc } from '@test/helpers/d2ArcDescribe';
+import { TEN_PERCENT } from '../../../src/constants';
+import { ITestContext } from '@test/helpers/d2ArcDescribe';
+import ArcNumber from '../../../src/utils/ArcNumber';
+import { D2ArcOptions } from '../../helpers/d2ArcDescribe';
 
-// let ownerAccount: Account;
-// let minterAccount: Account;
-// let otherAccount: Account;
+let ownerAccount: Account;
+let minterAccount: Account;
+let otherAccount: Account;
 
-// async function init(ctx: ITestContext): Promise<void> {
-//   [ownerAccount, minterAccount, otherAccount] = ctx.accounts;
-// }
+const expect = getWaffleExpect();
 
-// const expect = getWaffleExpect();
+const ctx: ITestContext = {};
 
-// d2ArcDescribe('D2Core.setters()', init, (ctx: ITestContext) => {
-//   before(async () => {});
+const COLLATERAL_AMOUNT = ArcNumber.new(200);
+const BORROW_AMOUNT = ArcNumber.new(50);
 
-//   describe('#withdrawTokens', () => {
-//     it('should not be callable by any user', async () => {});
+async function init(ctx: ITestContext): Promise<void> {
+  [ownerAccount, minterAccount, otherAccount] = ctx.accounts;
 
-//     it('should only be callable by the admin', async () => {});
-//   });
+  const setupOptions = {
+    oraclePrice: ArcDecimal.new(1).value,
+    collateralRatio: ArcDecimal.new(2).value,
+    interestRate: TEN_PERCENT,
+    initialCollateralBalances: [
+      [minterAccount, COLLATERAL_AMOUNT.mul(5)],
+      [otherAccount, COLLATERAL_AMOUNT.mul(5)],
+    ],
+  } as D2ArcOptions;
 
-//   describe('#setPause', () => {
-//     it('should not be callable by any user', async () => {});
+  await initializeD2Arc(ctx, setupOptions);
+}
 
-//     it('should only be callable by the admin', async () => {});
+describe('D2Core.admin', () => {
+  let ctx: ITestContext = {};
 
-//     it('should not be able to execute any action once paused', async () => {});
-//   });
-// });
+  before(async () => {
+    ctx = await d2Setup(init);
+
+    await ctx.arc.synth().collateral.mintShare(ctx.arc.coreAddress(), 100);
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  async function getCore(signer: Signer) {
+    return await ctx.arc.getCore(ctx.arc.synth(), signer);
+  }
+
+  describe('#withdrawTokens', () => {
+    it('should not be callable by any user', async () => {
+      const contract = await getCore(otherAccount.signer);
+      await expect(
+        contract.withdrawTokens(ctx.arc.synth().collateral.address, otherAccount.address, 100),
+      ).to.be.reverted;
+    });
+
+    it('should only be callable by the admin', async () => {
+      const contract = await getCore(ownerAccount.signer);
+      await contract.withdrawTokens(ctx.arc.synth().collateral.address, ownerAccount.address, 100);
+    });
+  });
+
+  describe('#setPause', () => {
+    it('should not be callable by any user', async () => {
+      const contract = await getCore(otherAccount.signer);
+      expect(await contract.paused()).to.be.false;
+      await expect(contract.setPause(true)).to.be.reverted;
+    });
+
+    it('should only be callable by the admin', async () => {
+      const contract = await getCore(ownerAccount.signer);
+      expect(await contract.paused()).to.be.false;
+      await contract.setPause(true);
+      expect(await contract.paused()).to.be.true;
+    });
+
+    it('should not be able to execute any action once paused', async () => {
+      const contract = await getCore(ownerAccount.signer);
+      expect(await contract.paused()).to.be.false;
+
+      await ctx.arc.openPosition(COLLATERAL_AMOUNT, BORROW_AMOUNT, minterAccount.signer);
+      await contract.setPause(true);
+      await expect(ctx.arc.openPosition(COLLATERAL_AMOUNT, BORROW_AMOUNT, minterAccount.signer)).to
+        .be.reverted;
+    });
+  });
+});
