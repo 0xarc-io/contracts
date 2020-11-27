087a364807aff1ed93334178b52d32ba510bcce5
Kerman Kohli <kermankohli@gmail.com>
2020-11-28 00:19:00 +1100

    feat(savings): tokenised savings without bugs/reduce complexity
    removed fee structure, removed use of signed int struct for balan

--- contracts/test/MockMozartSavingsV1.sol
+++ contracts/test/MockMozartSavingsV1.sol
@@ -3,27 +3,12 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
-import {MozartSavingsV1} from "../debt/mozart/MozartSavingsV1.sol";
+import {MozartSavingsV1} from "../debt/mozart/savings/MozartSavingsV1.sol";
 import {Decimal} from "../lib/Decimal.sol";
 
 contract MockMozartSavingsV1 is MozartSavingsV1 {
 
     uint256 private _currentTimestamp = 0;
-
-    constructor(
-        address _coreAddress,
-        address _accrualToken,
-        address _feeDestination,
-        Decimal.D256 memory _fee
-    )
-        MozartSavingsV1(
-            _coreAddress,
-            _accrualToken,
-            _feeDestination,
-            _fee
-        )
-        public
-    { }
 
     function setCurrentTimestamp(uint256 _timestamp)
         public
