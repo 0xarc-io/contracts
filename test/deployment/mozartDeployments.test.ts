418c7dda0fe77a9ab4ecc0f17603ef365e060596
Kerman Kohli <kermankohli@gmail.com>
2020-12-03 16:18:38 +1100

    chore: update config files for mainnet deployment

--- test/deployment/mozartDeployments.test.ts
+++ test/deployment/mozartDeployments.test.ts
@@ -3,7 +3,7 @@ import { loadContract } from '../../deployments/src/loadContracts';
 import { DeploymentType } from '../../deployments/src/writeToDeployments';
 import { generatedWallets } from '../helpers/generatedWallets';
 import { expect } from 'chai';
-import { MozartV1Factory } from '@src/typings';
+import { MozartV1Factory, SyntheticTokenV1Factory } from '@src/typings';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
@@ -23,7 +23,7 @@ function testNetwork(network: string) {
   const hreNetwork = hre.config.networks[network];
   const provider = new ethers.providers.JsonRpcProvider(hreNetwork.url);
   const signer = generatedWallets(provider)[0];
-  const isOwnerSet = (hreNetwork.users?.owner?.length > 0)
+  const isOwnerSet = hreNetwork.users?.owner?.length > 0;
   const ultimateOwner = hreNetwork.users?.owner.toLowerCase();
 
   const synths = ['ETHX'];
@@ -50,14 +50,21 @@ function testNetwork(network: string) {
       name: 'Oracle',
     });
 
-    // const mozartCore = MozartV1Factory.connect(coreProxyDetails.address, signer);
+    const mozartCore = MozartV1Factory.connect(coreProxyDetails.address, signer);
+    const synthetic = SyntheticTokenV1Factory.connect(syntheticProxyDetails.address, signer);
 
-    // it('should have the core configured correctly', async () => {
-    //   expect( (await mozartCore.getAdmin()).toLowerCase()).to.equal(ultimateOwner.toLowerCase());
-    //   expect((await mozartCore.getCurrentOracle()).toLowerCase()).to.equal(oracleDetails.address.toLowerCase());
-    //   expect((await mozartCore.getSyntheticAsset()).toLowerCase()).to.equal(syntheticProxyDetails.address.toLowerCase());
-    // })
+    it('should have the core configured correctly', async () => {
+      expect((await mozartCore.getAdmin()).toLowerCase()).to.equal(ultimateOwner.toLowerCase());
+      expect((await mozartCore.getCurrentOracle()).toLowerCase()).to.equal(
+        oracleDetails.address.toLowerCase(),
+      );
+      expect((await mozartCore.getSyntheticAsset()).toLowerCase()).to.equal(
+        syntheticProxyDetails.address.toLowerCase(),
+      );
+    });
 
-  })
-
+    it('should have the synthetic configured correctly', async () => {
+      expect(await (await synthetic.getAdmin()).toLowerCase()).to.equal(ultimateOwner);
+    });
+  });
 }
