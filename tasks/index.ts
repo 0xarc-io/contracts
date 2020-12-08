2a7bad56153c8a8326fbf1797ae6b0d3c693b174
Kerman Kohli <kermankohli@gmail.com>
2020-12-09 06:59:56 +1100

    feat: changeAdmin() and transferOwnership() tasks

--- tasks/index.ts
+++ tasks/index.ts
@@ -3,16 +3,10 @@ import 'module-alias/register';
 export * from './mozart-tasks';
 export * from './spritz-tasks';
 export * from './global-tasks';
-
-import path from 'path';
-import fs from 'fs-extra';
-import shell from 'shelljs';
+export * from './ownership-tasks';
+export * from './subgraph-tasks';
 
 import { task } from 'hardhat/config';
-import { gray } from 'chalk';
-import { constants, getPathToNetwork } from '../deployments.config';
-import { loadContract } from '../deployments/src/loadContracts';
-import { DeploymentType } from '../deployments/src/writeToDeployments';
 
 task('deploy-setup', 'Deploy all the smart contracts locally').setAction(async (taskArgs, hre) => {
   await hre.run('deploy-global');
@@ -22,42 +16,4 @@ task('deploy-setup', 'Deploy all the smart contracts locally').setAction(async (
   await hre.run('deploy-mozart', { synth: 'yUSD-STABLEx' });
   await hre.run('deploy-mozart-savings', { savings: 'STABLEx' });
   await hre.run('prepare-subgraph');
-});
-
-task('prepare-subgraph', 'Prepare a subgraph for deployment').setAction(async (taskArgs, hre) => {
-  const network = hre.network.name;
-
-  console.log(gray(`Loading the subgraph config for ${network.toUpperCase()}...`));
-
-  const subgraphFilePath = getPathToNetwork(network, constants.SUBGRAPH_CONFIG_FILENAME, path);
-  fs.ensureFileSync(subgraphFilePath);
-
-  const subgraphConfig = fs.readJSONSync(subgraphFilePath, { throws: false }) || {
-    network: 'mainnet',
-    startBlock: 0,
-  };
-
-  subgraphConfig.synthRegistry = loadContract({
-    network,
-    name: 'SynthRegistry',
-    type: DeploymentType.global,
-  }).address;
-
-  subgraphConfig.synthRegistryV2 = loadContract({
-    network,
-    name: 'SynthRegistryV2',
-    type: DeploymentType.global,
-  }).address;
-
-  try {
-    subgraphConfig.savingsRegistry = loadContract({
-      network,
-      name: 'SavingsRegistry',
-      type: DeploymentType.global,
-    }).address;
-  } catch {}
-
-  fs.writeFileSync(subgraphFilePath, JSON.stringify(subgraphConfig, null, 2));
-
-  shell.exec(`yarn mustache ${subgraphFilePath} subgraph.template.yaml subgraph.yaml`);
 });
