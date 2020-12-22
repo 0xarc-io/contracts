1712132c91f2781993a9635e6c55160c17bc184c
Kerman Kohli <kermankohli@gmail.com>
2020-12-22 11:24:26 +1100

    fix: borrow test

--- test/contracts/mozart/borrow.test.ts
+++ test/contracts/mozart/borrow.test.ts
@@ -49,14 +49,14 @@ describe('Mozart.operateAction(Borrow)', () => {
     expect(await arc.synth().core.isCollateralized(postPosition, price)).to.be.true;
   });
 
-  it.only('should update the index', async () => {
+  it('should update the index', async () => {
     // Set the time to one year from now in order for interest to accumulate
     const totalBorrowed0 = (await arc.core().getTotals())[1];
 
     await arc.updateTime(ONE_YEAR_IN_SECONDS);
     await arc.synth().core.updateIndex();
 
-    let borrowIndex = await arc.core().getBorrowIndex();
+    const borrowIndex1 = await arc.core().getBorrowIndex();
     const totalBorrowed1 = (await arc.core().getTotals())[1];
 
     // In order to calculate the new index we need to multiply one year
@@ -66,8 +66,8 @@ describe('Mozart.operateAction(Borrow)', () => {
     );
 
     // Our calculated index should equal the newly set borrow index
-    expect(borrowIndex[0]).to.equal(calculatedIndex);
-    expect(totalBorrowed1).to.equal(ArcNumber.bigMul(totalBorrowed0, borrowIndex[0]));
+    expect(borrowIndex1[0]).to.equal(calculatedIndex);
+    expect(totalBorrowed1).to.equal(ArcNumber.bigMul(totalBorrowed0, borrowIndex1[0]));
 
     // Set the time to two years from now in order for interest to accumulate
     await arc.updateTime(ONE_YEAR_IN_SECONDS.mul(2));
@@ -75,15 +75,14 @@ describe('Mozart.operateAction(Borrow)', () => {
 
     const totalBorrowed2 = (await arc.core().getTotals())[1];
 
-    borrowIndex = await arc.synth().core.getBorrowIndex();
+    const borrowIndex2 = await arc.synth().core.getBorrowIndex();
 
     const earnedInterest = (await arc.synth().core.getInterestRate()).mul(ONE_YEAR_IN_SECONDS);
     calculatedIndex = calculatedIndex.add(earnedInterest);
 
     // Our calculated index should equal the newly set borrow index
-    expect(borrowIndex[0]).to.equal(calculatedIndex);
-
-    expect(totalBorrowed2).to.equal(ArcNumber.bigMul(borrowIndex[0], earnedInterest.add(BASE)));
+    expect(borrowIndex2[0]).to.equal(calculatedIndex);
+    expect(totalBorrowed2).to.equal(ArcNumber.bigMul(totalBorrowed1, earnedInterest.add(BASE)));
   });
 
   it('should be able to borrow more if the c-ratio is not at the minimum', async () => {
