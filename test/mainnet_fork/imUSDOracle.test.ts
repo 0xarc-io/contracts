9f311a4021893e0c5d9d778251be78dd0d40bcdd
Gabriel T <g@arcx.money>
2021-01-22 19:54:02 -0500

    test(imusdoracle.test.ts): import typings

--- test/mainnet_fork/imUSDOracle.test.ts
+++ test/mainnet_fork/imUSDOracle.test.ts
@@ -1,13 +1,14 @@
+import { ImUSDOracle, ImUSDOracleFactory } from '@src/typings';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-xdescribe('imUSDOracle', () => {
-  let oracle: imUSDOracle;
+describe.skip('imUSDOracle', () => {
+  let oracle: ImUSDOracle;
 
   before(async () => {
     const signer = await ethers.provider.getSigner();
 
-    oracle = await new imUSDOracleFactory(signer).deploy({ gasLimit: 10000000 });
+    oracle = await new ImUSDOracleFactory(signer).deploy({ gasLimit: 10000000 });
     console.log('oracle address:', oracle.address);
   });
 
