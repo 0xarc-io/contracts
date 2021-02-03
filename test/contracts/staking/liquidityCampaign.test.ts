30b8800d30c82547fc20d064317311d805b72ea9
Gabriel T <g@arcx.money>
2021-02-03 18:57:41 -0500

    test(liquiditycampaign.test.ts): add tests placeholders for the liquidity campaign

---
+++ test/contracts/staking/liquidityCampaign.test.ts
@@ -0,0 +1,43 @@
+describe('LiquidityCampaign', () => {
+  describe('#stake', () => {
+    xit('should not be able to stake more than balance')
+
+    xit('should be able to stake')
+  })
+
+  describe('#getReward', () => {
+    xit('should not be able to claim more rewards past the end date')
+
+    xit('should be able to claim rewards gradually over time')
+
+    xit('should be able to claim the right amount of rewards given the number of participants')
+  })
+
+  describe('#withdraw', () => {
+    xit('should be able to withdraw')
+  })
+  
+  describe('#exit', () => {
+    xit('should be able to exit and get the right amount of staked tokens and rewards')
+  })
+
+  describe('#notifyRewardAmount', () => {
+    xit('should not be callable by anyone')
+
+    xit('should only be callable by the rewards distributor')
+  })
+
+  describe('#recoverERC20', () => {
+    xit('should not be callable by anyone')
+    
+    xit('should not recover staking or reward token')
+    
+    xit('should let admin recover the erc20 on this contract')
+  })
+
+  describe('#init', () => {
+    xit('should not be callable by anyone')
+
+    xit('should only be callable by the contract owner')
+  })
+})
