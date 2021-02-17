41086416eae1e44b7606c505a31c63d1b5140f68
Gabriel T <g@arcx.money>
2021-02-17 13:31:59 -0500

    feat(staking-tasks.ts): add deployment script with auto validation

--- tasks/staking-tasks.ts
+++ tasks/staking-tasks.ts
@@ -15,6 +15,7 @@ import {
   pruneDeployments,
 } from '../deployments/src';
 import { task } from 'hardhat/config';
+import { run } from 'hardhat';
 
 task('deploy-staking', 'Deploy a staking/reward pool')
   .addParam('name', 'The name of the pool you would like to deploy')
@@ -281,5 +282,102 @@ task('deploy-staking-liquidity', 'Deploy a LiquidityCampaign')
       console.log(green('Rewards duration successfully set'));
     } catch (error) {
       console.log(red(`Failed to set the rewards duration. Reason: ${error}\n`));
+    }
+  });
+
+task('deploy-staking-joint', 'Deploy a JointCampaign')
+  .addParam('name', 'The name of the pool you would like to deploy')
+  .addOptionalParam('verifyPath', 'Path to the contract to verify on etherscan')
+  .setAction(async (taskArgs, hre) => {
+    const name = taskArgs.name;
+    const contractPath = taskArgs.verifyPath;
+
+    const { network, signer, networkConfig, networkDetails } = await loadDetails(taskArgs, hre);
+    const ultimateOwner = networkDetails['users'].owner.toLowerCase();
+    const stakingConfig = await loadStakingConfig({ network, key: name });
+
+    if (!stakingConfig.rewardsDurationSeconds) {
+      throw red(`"rewardsDurationSeconds" is not set in the staking-config.ts file`);
+    }
+
+    await pruneDeployments(network, signer.provider);
+
+    const arcDAO = await loadContract({
+      name: 'ArcDAO',
+      type: DeploymentType.global,
+      network,
+    });
+
+    const rewardToken = await loadContract({
+      name: stakingConfig.rewardsToken,
+      type: DeploymentType.global,
+      network,
+    });
+
+    if (!arcDAO || !rewardToken) {
+      throw red(`There is no ArcDAO, RewardToken or Core Contract in this deployments file`);
+    }
+
+    if (!stakingConfig.contractFactory) {
+      throw red(`"contractFactory" missing from in the staking-config.ts file`);
+    }
+
+    const stakingTokenAddress =
+      stakingConfig.stakingToken ||
+      (await deployContract(
+        {
+          name: 'TestStakingToken',
+          source: 'TestToken',
+          data: new TestTokenFactory(signer).getDeployTransaction(`${name}-TestToken`, name, 18),
+          version: 1,
+          type: DeploymentType.staking,
+          group: name,
+        },
+        networkConfig,
+      ));
+
+    // Joint campaign doesn't use a proxy
+    const jointCampaignAddress = await deployContract(
+      {
+        name: 'JointCampaign',
+        source: stakingConfig.source,
+        data: stakingConfig.getDeployTx(signer),
+        type: DeploymentType.staking,
+        group: name,
+        version: 1,
+      },
+      networkConfig,
+    );
+
+    const jointCampaignContract = stakingConfig.contractFactory.connect(
+      jointCampaignAddress,
+      signer,
+    );
+
+    try {
+      console.log(yellow(`* Calling init()...`));
+      await stakingConfig.runInit(
+        jointCampaignContract,
+        arcDAO.address,
+        ultimateOwner,
+        rewardToken.address,
+      );
+      console.log(green(`Called init() successfully!\n`));
+    } catch (error) {
+      console.log(red(`Failed to call init().\nREason: ${error}\n`));
+    }
+
+    try {
+      console.log(yellow(`Setting rewards duration...`));
+      await jointCampaignContract.setRewardsDuration(stakingConfig.rewardsDurationSeconds);
+      console.log(green(`Rewards duration successfully set`));
+    } catch (error) {
+      console.log(red(`Failed to set the rewards duration. Reason: ${error}\n`));
+    }
+
+    if (contractPath) {
+      console.log(green(`Verifying contract at address ${jointCampaignAddress}`));
+
+      await run('verify-contract', { path: contractPath, address: jointCampaignAddress });
     }
   });
