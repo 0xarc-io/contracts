ebe36bffd0454b34170cd0c48580cceaa82f7e4a
Gabriel T <g@arcx.money>
2021-02-18 22:04:02 -0500

    feat(wstethoracle.sol): implement wstETH oracle

---
+++ test/mainnet_fork/wstEthOracle.test.ts
@@ -0,0 +1,20 @@
+import { WstEthOracle, WstEthOracleFactory } from '@src/typings';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe.skip('WstEthOracle', () => {
+  let oracle: WstEthOracle;
+
+  before(async () => {
+    const signer = await ethers.provider.getSigner();
+
+    oracle = await new WstEthOracleFactory(signer).deploy({ gasLimit: 10000000 });
+    console.log('oracle address:', oracle.address);
+  });
+
+  it('should give the correct price', async () => {
+    const price = await oracle.fetchCurrentPrice();
+    console.log('price:', price.value.toString());
+    expect(price).to.not.be.null;
+  });
+});
