0e027111fd3fe7b9d07dd3e12124d1d0c28dea95
Kerman Kohli <kermankohli@gmail.com>
2021-02-19 17:41:14 +1100

    feat: setup first test for credit score contract

--- test/contracts/sapphire/creditScore.test.ts
+++ test/contracts/sapphire/creditScore.test.ts
@@ -1,17 +1,38 @@
+import chai, { expect } from 'chai';
+import { solidity } from 'ethereum-waffle';
 import 'module-alias/register';
+import { generateContext, ITestContext } from '../context';
+import { sapphireFixture } from '../fixtures';
+
+chai.use(solidity);
 
 /**
  * This is the core credit score contract and is where a user's credit score will
  * be posted. The logic around this contract needs to be very sound since we anticipate
  * it to be a core DeFi primitive for other applications to build on.
  */
-describe('SapphireCreditScore', () => {
+describe.only('SapphireCreditScore', () => {
   // Utils will need to be developed to generate valid + invalid merkle roots
+  let ctx: ITestContext;
+
+  before(async () => {
+    ctx = await generateContext(sapphireFixture, async () => {});
+  });
+
+  it('should have merkle root updater not equal owner',  async () => {
+    const merkleRootUpdater = await ctx.contracts.sapphire.creditScore.merkleRootUpdater();
+    expect(merkleRootUpdater).not.eq(ctx.signers.admin.address);
+    expect(merkleRootUpdater).eq(ctx.signers.interestSetter.address);
+  })
 
   describe('#updateMerkleRoot', () => {
-    it('should not be able to update the merkle root as an unauthorised user', async () => {});
+    it('should not be able to update the merkle root as an unauthorised user', async () => {
+      await expect(ctx.contracts.sapphire.creditScore.connect(ctx.signers.unauthorised).updateMerkleRoot('0x0000000000000000000000000000000000000000000000000000000000000000')).to.be.revertedWith('SapphireCreditScore: caller is not authorized to update merkle root');
+    });
 
-    it('should not be able to update as the owner if the contract is not paused', async () => {});
+    it('should not be able to update as the owner if the contract is not paused', async () => {
+      await expect(ctx.contracts.sapphire.creditScore.updateMerkleRoot('0x0000000000000000000000000000000000000000000000000000000000000000')).to.be.revertedWith('SapphireCreditScore: pause contract to update merkle root as owner');
+    });
 
     it('should not be able to be called by the root updater before the delay duration', async () => {});
 
