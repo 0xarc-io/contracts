b670519f2c10c4c9d5c7ff62d5932dabc5b9d547
Kerman Kohli <kermankohli@gmail.com>
2021-02-19 17:41:14 +1100

    chore: skeleton

---
+++ contracts/debt/sapphire/SapphireMapperLinear.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+
+contract SapphireMapperLinear {
+
+    function map(
+        uint256 score,
+        uint256 variableOne,
+        uint256 variableTwo
+    )
+        public
+        returns (uint256)
+    {
+        // Sort the two vars, highest first
+        // Map linearly
+        // Return the result
+    }
+
+}
