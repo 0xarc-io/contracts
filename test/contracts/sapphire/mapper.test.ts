9aa4cc9926bb7912e62cba24a4aa37c751250c24
Gabriel T <g@arcx.money>
2021-03-01 21:00:26 -0500

    feat(isapphiremapper.sol): add interface of sapphire mapper and tests for sapphire mapper linear
    test(mapper.test.ts): forgot to add signer
    
    feat(sapphiremapperlinear.sol): implement the sapphire linear mapper

---
+++ test/contracts/sapphire/mapper.test.ts
@@ -0,0 +1,46 @@
+import { SapphireMapperLinear, SapphireMapperLinearFactory } from '@src/typings';
+import ArcNumber from '@src/utils/ArcNumber';
+import { expectRevert } from '@test/helpers/expectRevert';
+import { expect } from 'chai';
+import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+
+describe('SapphireMapperLinear', () => {
+  let mapper: SapphireMapperLinear;
+
+  before(async () => {
+    const signers = await ethers.getSigners();
+    const admin = signers[0];
+    mapper = await new SapphireMapperLinearFactory(admin).deploy();
+  });
+
+  it('reverts if scoreMax or upperbound are 0', async () => {
+    await expectRevert(mapper.map(0, 0, 1, 10));
+    await expectRevert(mapper.map(0, 5, 0, 0));
+    await expectRevert(mapper.map(0, 0, 0, 0));
+  });
+
+  it('reverts if the lower bound and the upper bound are equal', async () => {
+    await expectRevert(mapper.map(0, 0, 5, 5));
+  });
+
+  it('reverts if the upper bound is smaller than the lower bound', async () => {
+    await expectRevert(mapper.map(0, 0, 5, 4));
+  });
+
+  it('reverts if score is bigger than the maximum score', async () => {
+    await expectRevert(mapper.map(6, 5, 1, 10));
+  });
+
+  it('returns a correct score mapping', async () => {
+    expect(await mapper.map(0, 1, 0, 1)).to.eq(BigNumber.from(0));
+
+    expect(await mapper.map(5, 10, 0, 100)).to.eq(BigNumber.from(50));
+
+    expect(await mapper.map(10, 10, 0, 100)).to.eq(BigNumber.from(100));
+
+    expect(await mapper.map(ArcNumber.new(65), ArcNumber.new(100), 0, ArcNumber.new(1000))).to.eq(
+      ArcNumber.new(650),
+    );
+  });
+});
