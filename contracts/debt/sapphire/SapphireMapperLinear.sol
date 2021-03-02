e2fd4b1bb89d6f434c3fbc511e675be687a64113
Gabriel T <g@arcx.money>
2021-03-01 21:00:26 -0500

    feat(isapphiremapper.sol): add interface of sapphire mapper and tests for sapphire mapper linear

--- contracts/debt/sapphire/SapphireMapperLinear.sol
+++ contracts/debt/sapphire/SapphireMapperLinear.sol
@@ -2,19 +2,24 @@
 
 pragma solidity ^0.5.16;
 
-contract SapphireMapperLinear {
+import {ISapphireMapper} from "./ISapphireMapper.sol";
+
+contract SapphireMapperLinear is ISapphireMapper {
 
     function map(
-        uint256 score,
-        uint256 variableOne,
-        uint256 variableTwo
+        uint256 _score,
+        uint256 _scoreMax,
+        uint256 _lowerBound,
+        uint256 _upperBound
     )
         public
+        view
         returns (uint256)
     {
         // Sort the two vars, highest first
         // Map linearly
         // Return the result
+        return 0;
     }
 
 }
