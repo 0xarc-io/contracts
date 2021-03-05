5a1e035e1db2821f10c137c63c52c06c7b162c45
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2021-03-05 08:47:32 +0300

    feat: implement #request

--- contracts/debt/sapphire/ISapphireCreditScore.sol
+++ contracts/debt/sapphire/ISapphireCreditScore.sol
@@ -10,7 +10,7 @@ interface ISapphireCreditScore {
     
     function updateMerkleRootUpdater(address mercleRootUpdator) external;
 
-    function request(SapphireTypes.ScoreProof calldata proof) external view returns (uint256);
+    function request(SapphireTypes.ScoreProof calldata proof) external returns (uint256);
 
     function getLastScore(address user) external view returns (uint256, uint256);
 
