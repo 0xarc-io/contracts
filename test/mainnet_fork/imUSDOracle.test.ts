9cf735d7c68915b98a4c7c7e74ad3c3436ef270d
Gabriel T <g@arcx.money>
2021-03-15 12:50:36 -0400

    refactor: moved the ganache fork URL to .env

--- test/mainnet_fork/imUSDOracle.test.ts
+++ test/mainnet_fork/imUSDOracle.test.ts
@@ -8,7 +8,7 @@ describe('imUSDOracle', () => {
   before(async () => {
     const provider = new MockProvider({
       ganacheOptions: {
-        fork: 'https://eth-mainnet.alchemyapi.io/v2/HSgFSArdYblhAJVgM8F820KLd65jiFzc',
+        fork: 'process.env.GANACHE_FORK_URL/v2/HSgFSArdYblhAJVgM8F820KLd65jiFzc',
         fork_block_number: 12025602,
       },
     });
