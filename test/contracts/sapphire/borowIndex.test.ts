08f04840e709ff6d8de94e15bd0eae0ad0a0406d
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2021-03-17 14:13:10 +0300

    feat: update descriptions for borrow index tests

--- test/contracts/sapphire/borowIndex.test.ts
+++ test/contracts/sapphire/borowIndex.test.ts
@@ -9,21 +9,21 @@ describe('borrowIndex', () => {
     it('for two years', () => {});
   });
 
-  describe('calculate the index for opening two position', () => {
+  describe('calculate the index for opening two positions', () => {
     it('the first for one year, the second for half a year', () => {});
 
-    it('the first for one and half years, the second for 3 months', () => {});
+    it('the first for one and a half years, the second for 3 months', () => {});
 
     it('the first for two years, the second for one and half years', () => {});
   });
 
   describe('calculate the index for 2 years for opening and updating a position', () => {
-    it('open for 1 year and borrow more for half a year', () => {});
+    it('open for 1 year and borrow more after this year', () => {});
 
-    it('open for 1 year and liquidate after a year', () => {});
+    it('open for 1 year and liquidate after this year', () => {});
 
-    it('open for 1 year and repay partially after a year', () => {});
+    it('open for 1 year and repay partially after this year', () => {});
 
-    it('open for 1 year and repay fully after a year', () => {});
+    it('open for 1 year and repay fully after this year', () => {});
   });
 });
