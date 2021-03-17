a5cdfd9e8547577ace9155692bf91685953b15f8
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2021-03-17 10:03:59 +0300

    feat: change interest rate to index

--- test/contracts/sapphire/borowIndex.test.ts
+++ test/contracts/sapphire/borowIndex.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 describe('borrowIndex', () => {
-  describe('calculate the interest rate for opening a position', () => {
+  describe('calculate the index for opening a position', () => {
     it('for one year', () => {});
 
     it('for one and half years', () => {});
@@ -9,7 +9,7 @@ describe('borrowIndex', () => {
     it('for two years', () => {});
   });
 
-  describe('calculate the interest rate for opening two position', () => {
+  describe('calculate the index for opening two position', () => {
     it('the first for one year, the second for half a year', () => {});
 
     it('the first for one and half years, the second for 3 months', () => {});
@@ -17,7 +17,7 @@ describe('borrowIndex', () => {
     it('the first for two years, the second for one and half years', () => {});
   });
 
-  describe('calculate the interest rate for 2 years for opening and updating a position', () => {
+  describe('calculate the index for 2 years for opening and updating a position', () => {
     it('open for 1 year and borrow more for half a year', () => {});
 
     it('open for 1 year and liquidate after a year', () => {});
