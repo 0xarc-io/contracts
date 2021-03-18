78263e2b5144193e77b74819838133b1fdcea4ca
Gabriel T <g@arcx.money>
2021-03-18 14:59:52 -0400

    test(liquidate.test.ts): add liquidate test descriptions

--- test/contracts/sapphire/liquidate.test.ts
+++ test/contracts/sapphire/liquidate.test.ts
@@ -7,7 +7,7 @@ import 'module-alias/register';
  * an insta profit by selling the collateral they got a discount.
  */
 describe('SapphireCore.liquidate()', () => {
-  it('should be able to liquidate an undercollateralized position', async () => {
+  it('liquidates an undercollateralized position', async () => {
     // When a liquidation is done we need to check the following
     // - Ensure that the liquidator has enough debt (STABLEx)
     // - Ensure the position is under-collateralized
@@ -20,42 +20,55 @@ describe('SapphireCore.liquidate()', () => {
     // - A portion of collateral is sent to the fee collector
   });
 
-  it('should be able to provide a score proof and then liquidate the position', async () => {
+  it('provides a score proof and then liquidates the position', async () => {
     // Setup a test which has a valid position
     // The user's credit score decreases
     // The liquidator submits the user's credit score and is then able to liquidate
   });
 
-  it('should be able to liquidate if interest accumulates (1 day)', async () => {
+  it('liquidates if interest accumulates (1 day)', async () => {
     // Open a position at the boundary
     // Test that a liquidation will occur if the user accumulates enough debt via interest
   });
 
-  it('should be able to liquidate if interest accumulates (1 year)', async () => {
+  it('liquidates if interest accumulates (1 year)', async () => {
     // Open a position at the boundary
     // Test that a liquidation will occur if the user accumulates enough debt via interest
   });
 
-  it('should be able to liquidate if the price drops', async () => {
+  it('liquidates if the price drops', async () => {
     // Update the price to below the position's collateral ratio
     // Check if the position was liquidated correctly
   });
 
-  it('should be able to liquidate again if the price drops', async () => {
-    // If the price drops twice, the position should be able to be liquidated twice
+  it('liquidates again if the price drops', async () => {
+    // If the price drops twice, the position bes liquidated twice
   });
 
-  it('should be able to liquidate the remains if the price crashes by a large amount', async () => {
+  it('liquidates the remains if the price crashes by a large amount', async () => {
     // If the price drops more than the value of the collateral
     // A liquidator can take whatever remaining value is in the position
   });
 
-  it('should not be able to liquidate a collateralized position ', async () => {});
+  it('should not liquidate a collateralized position ', async () => {});
 
-  // @TODO:Some case for when the user is under-collateralised, but their credit score increases
-  // making them immune to the liquidation and causing it to throw. Not sure what to call this.
+  it('should not liquidate if the credit score improved such that vault is immune to liquidations', async () => {
+    /**
+     * The user is under-collateralised, but their credit score increases
+     * making them immune to the liquidation and causing it to throw
+     */
+  });
 
-  it('should not be able to liquidate without enough debt', async () => {});
+  it('should not liquidate without enough debt', async () => {});
 
-  it('should not be able to liquidate if the price increases', async () => {});
+  it('should not liquidate if the price increases', async () => {});
+
+  it('should not liquidate twice in a row', async () => {
+    /**
+     * Given the price does not change, there isn't a lot of time elapsed and the credit score
+     * did not drop significantly, someone should not be able to liquidate twice or more in a row
+     */
+    // 1. Liquidate
+    // 2. Liquidate again -> expect revert: position is collateralized
+  });
 });
