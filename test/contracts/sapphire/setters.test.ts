09303fb7f4b7fa36122c8bebff692b8a5b4c9d6b
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2021-04-07 19:03:28 +0300

    test(setters.test): setCollateralRatios

--- test/contracts/sapphire/setters.test.ts
+++ test/contracts/sapphire/setters.test.ts
@@ -1,11 +1,58 @@
+import { SapphireCoreV1 } from '@src/typings';
+import { expect } from 'chai';
+import { constants } from 'ethers';
+import { generateContext, ITestContext } from '../context';
+import { sapphireFixture } from '../fixtures';
+import { setupSapphire } from '../setup';
+
 describe('SapphireCore.setters', () => {
+  let ctx: ITestContext;
+  let sapphireCore: SapphireCoreV1;
+
+  before(async () => {
+    ctx = await generateContext(sapphireFixture, (ctx) => setupSapphire(ctx, {}));
+  });
+
   describe('#setCollateralRatios', () => {
-    it('reverts if called by non-owner');
-    it('reverts if low c-ratio is 0');
-    it('reverts if high c-ratio is 0');
-    it('reverts if high c-ratio is lower than the low c-ratio');
-    it('sets the low and high collateral ratios');
-    it('emits the CollateralRatiosUpdated event');
+    const lowRatio = constants.WeiPerEther.mul(3);
+    const highRatio = constants.WeiPerEther.mul(5);
+
+    it('reverts if called by non-owner', async () => {
+      await expect(
+        sapphireCore.connect(ctx.signers.unauthorised).setCollateralRatios(lowRatio, highRatio),
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('reverts if low c-ratio is 0', async () => {
+      await expect(sapphireCore.setCollateralRatios(0, highRatio)).to.be.revertedWith(
+        'SapphireCoreV1: collateral ratio has to be greater than 0',
+      );
+    });
+
+    it('reverts if high c-ratio is 0', async () => {
+      await expect(sapphireCore.setCollateralRatios(lowRatio, 0)).to.be.revertedWith(
+        'SapphireCoreV1: collateral ratio has to be greater than 0',
+      );
+    });
+
+    it('reverts if high c-ratio is lower than the low c-ratio', async () => {
+      await expect(sapphireCore.setCollateralRatios(highRatio, lowRatio)).to.be.revertedWith(
+        'SapphireCoreV1: high c-ratio is lower than the low c-ratio',
+      );
+    });
+
+    it('sets the low and high collateral ratios', async () => {
+      expect(await sapphireCore.highCollateralRatio()).not.eq(highRatio);
+      expect(await sapphireCore.lowCollateralRatio()).not.eq(lowRatio);
+
+      const transactionPromise = sapphireCore.setCollateralRatios(lowRatio, highRatio);
+      await expect(transactionPromise)
+        .to.emit(sapphireCore, 'CollateralRatiosUpdated')
+        .withArgs(lowRatio, highRatio);
+
+      expect(await sapphireCore.highCollateralRatio()).eq(highRatio);
+      expect(await sapphireCore.lowCollateralRatio()).eq(lowRatio);
+    });
   });
 
   describe('#setcollateralRatioAssessor', () => {
