850db098b2d00b8565f500c098398a743c0f6dba
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2021-04-13 16:29:59 +0200

    fix(borrow.test): most tests with success

--- test/helpers/sapphireDefaults.ts
+++ test/helpers/sapphireDefaults.ts
@@ -3,7 +3,7 @@ import { BigNumber, utils } from 'ethers';
 export const DEFAULT_HiGH_C_RATIO = utils.parseEther('2');
 export const DEFAULT_LOW_C_RATIO = utils.parseEther('1');
 
-export const DEFAULT_PRICE = utils.parseEther('1');
+export const DEFAULT_PRICE = utils.parseEther('10');
 
 export const DEFAULT_COLLATERAL_DECIMALS = BigNumber.from(6);
 
