79d01197e88bb9eb708e9fdaa06429de37c27b61
Yaraslau Stsetskevich <yaraslau.s@gmail.com>
2021-04-19 17:17:40 +0300

    refactor(setup vault): extract mintApprovedCollateral

--- test/helpers/setupBaseVault.ts
+++ test/helpers/setupBaseVault.ts
@@ -2,8 +2,8 @@ import { CreditScoreProof } from '@arc-types/sapphireCore';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { SapphireTestArc } from '@src/SapphireTestArc';
 import { TestTokenFactory } from '@src/typings';
-import { utils } from 'ethers';
 import { DEFAULT_COLLATERAL_DECIMALS } from './sapphireDefaults';
+import { BigNumberish, utils } from 'ethers';
 
 /**
  * Setup base **Sapphire** vault with 1000 collateral and 200 debt for a c-ratio of
@@ -12,16 +12,24 @@ import { DEFAULT_COLLATERAL_DECIMALS } from './sapphireDefaults';
 export async function setupBaseVault(
   arc: SapphireTestArc,
   caller: SignerWithAddress,
-  collateralAmt = utils.parseUnits('1000', DEFAULT_COLLATERAL_DECIMALS),
-  borrowAmt = utils.parseEther('200'),
+  collateralAmount = utils.parseUnits('1000', DEFAULT_COLLATERAL_DECIMALS),
+  borrowAmount = utils.parseEther('200'),
   scoreProof?: CreditScoreProof,
   synthName?: string,
 ) {
-  const collateralContract = TestTokenFactory.connect(arc.collateral().address, caller);
-
-  await collateralContract.mintShare(caller.address, collateralAmt);
-  await collateralContract.approve(arc.core().address, collateralAmt);
+  await mintApprovedCollateral(arc, caller, collateralAmount);
 
   // Open vault and mint debt
-  return arc.open(collateralAmt, borrowAmt, scoreProof, synthName, caller);
+  return arc.open(collateralAmount, borrowAmount, scoreProof, synthName, caller);
+}
+
+export async function mintApprovedCollateral(
+  arc: SapphireTestArc,
+  caller: SignerWithAddress,
+  collateralAmount: BigNumberish,
+) {
+  const collateralContract = TestTokenFactory.connect(arc.collateral().address, caller);
+
+  await collateralContract.mintShare(caller.address, collateralAmount);
+  await collateralContract.approve(arc.core().address, collateralAmount);
 }
