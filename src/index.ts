5f1fcd9e35d4282d38ebd6086e82d0fd92194e03
Gabriel T <g@arcx.money>
2021-06-04 16:36:05 -0400

    chore: add exports required for sapphire

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from './SpritzArc';
 export * from './MozartArc';
 export * from './SpritzTestArc';
 export * from './constants';
+export * from './MerkleTree'
+export * from './utils'
 export { approve } from './utils/approve';
