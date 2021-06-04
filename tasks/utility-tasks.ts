bcd84a1337eb1920217397498f2a44648acabb8d
Gabriel T <g@arcx.money>
2021-06-04 15:07:52 -0400

    feat(utility-tasks.ts): add utility to approve token if it was not verified on etherscan

---
+++ tasks/utility-tasks.ts
@@ -0,0 +1,55 @@
+import { IERC20Factory } from '@src/typings/IERC20Factory';
+import { IMintableTokenFactory } from '@src/typings/IMintableTokenFactory';
+import { green, yellow } from 'chalk';
+import { loadDetails } from 'deployments/src';
+import { utils } from 'ethers';
+import { task } from 'hardhat/config';
+
+task('mint-tokens')
+  .addParam('token', 'The address of the token to mint from')
+  .addParam('to', 'The receiver of the tokens')
+  .addParam('amount', 'The amount of tokens, in reduced format (10^18)')
+  .setAction(async (taskArgs, hre) => {
+    const tokenAddress = taskArgs['token'];
+    const receiver = taskArgs['to'];
+    const amount = taskArgs['amount'];
+
+    const { network, signer } = await loadDetails(taskArgs, hre);
+
+    const token = IMintableTokenFactory.connect(tokenAddress, signer);
+
+    // mint to deployer
+    console.log(
+      yellow(
+        `Minting ${amount}*10^18 of ${tokenAddress} tokens to ${receiver}...`,
+      ),
+    );
+    const tx = await token.mint(receiver, utils.parseEther(amount));
+    console.log(yellow(`tx hash: ${tx.hash}`));
+
+    await tx.wait();
+
+    console.log(green(`tx completed`));
+  });
+
+/**
+ * Useful to approve tokens on Rinkeby when a token contract was not
+ * verified on Etherscan. For example, approving DAI to apply to
+ * a waitlist batch
+ */
+task('approve-tokens')
+  .addParam('token', 'The address of the token')
+  .addParam('spender', 'The address of the spender')
+  .addParam('amount', 'The amount to approve, in ether form')
+  .setAction(async (taskArgs, hre) => {
+    const { token, spender, amount } = taskArgs;
+
+    const { signer } = await loadDetails(taskArgs, hre);
+
+    const erc20 = IERC20Factory.connect(token, signer);
+
+    console.log(yellow(`Approving token...`));
+    const tx = await erc20.approve(spender, utils.parseEther(amount));
+    await tx.wait();
+    console.log(green(`Token approved!`));
+  });
