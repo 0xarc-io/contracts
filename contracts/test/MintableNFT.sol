bf2520d9a6b5e755b62f748fb77c29cbf22739ff
Gabriel T <g@arcx.money>
2021-06-18 15:36:27 -0400

    test(defipassport.test.ts): pass set active skin test

--- contracts/test/MintableNFT.sol
+++ contracts/test/MintableNFT.sol
@@ -4,7 +4,7 @@ import {ERC721Full} from "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import {ERC721Mintable} from "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
 contract MintableNFT is ERC721Full, ERC721Mintable {
-    
+
     constructor (
         string memory _name,
         string memory _symbol
