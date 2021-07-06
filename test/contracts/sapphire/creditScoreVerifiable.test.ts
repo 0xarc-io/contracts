0bffd98c3cc0f558d7e27d4cb80329a754fd6b61
Gabriel T <g@arcx.money>
2021-07-05 21:15:28 -0400

    test(creditscoreverifiable.sol): add tests for CreditScoreVerifiable

---
+++ test/contracts/sapphire/creditScoreVerifiable.test.ts
@@ -0,0 +1,120 @@
+import { CreditScore } from '@arc-types/sapphireCore';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { CreditScoreTree } from '@src/MerkleTree';
+import { SapphireCreditScore } from '@src/typings';
+import { CreditScoreVerifiableTest } from '@src/typings/CreditScoreVerifiableTest';
+import { CreditScoreVerifiableTestFactory } from '@src/typings/CreditScoreVerifiableTestFactory';
+import { getEmptyScoreProof, getScoreProof } from '@src/utils';
+import { addSnapshotBeforeRestoreAfterEach } from '@test/helpers/testingUtils';
+import { expect } from 'chai';
+import { BigNumber, ethers } from 'ethers';
+import { initial } from 'lodash';
+import { generateContext } from '../context';
+import { sapphireFixture } from '../fixtures';
+import { setupSapphire } from '../setup';
+
+describe('CreditScoreVerifiable', () => {
+  let creditScoreContract: SapphireCreditScore;
+  let contract: CreditScoreVerifiableTest;
+  let owner: SignerWithAddress;
+  let user: SignerWithAddress;
+
+  let userCreditScore: CreditScore;
+  let ownerCreditScore: CreditScore;
+  let tree: CreditScoreTree;
+
+  before(async () => {
+    const ctx = await generateContext(sapphireFixture, async (ctx) => {
+      userCreditScore = {
+        account: ctx.signers.scoredMinter.address,
+        amount: BigNumber.from(12),
+      };
+      ownerCreditScore = {
+        account: ctx.signers.admin.address,
+        amount: BigNumber.from(20),
+      };
+      tree = new CreditScoreTree([userCreditScore, ownerCreditScore]);
+      return setupSapphire(ctx, {
+        merkleRoot: tree.getHexRoot(),
+      });
+    });
+    owner = ctx.signers.admin;
+    user = ctx.signers.scoredMinter;
+
+    creditScoreContract = ctx.contracts.sapphire.creditScore;
+    contract = await new CreditScoreVerifiableTestFactory(owner).deploy(
+      creditScoreContract.address,
+    );
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe('#constructor', () => {
+    it('reverts if the credit score contract passed is not an address', async () => {
+      await expect(
+        new CreditScoreVerifiableTestFactory(owner).deploy(
+          ethers.Wallet.createRandom().address,
+        ),
+      ).to.be.revertedWith(
+        'CreditScoreVerifiable: the credit score passed is not a contract',
+      );
+    });
+
+    it('sets the credit score contract', async () => {
+      expect(await contract.creditScoreContract()).to.eq(
+        creditScoreContract.address,
+      );
+    });
+  });
+
+  describe('#checkScoreProof', () => {
+    it('reverts if the score proof is required but not passed', async () => {
+      await expect(
+        contract.proofRequiredDoSomething(getEmptyScoreProof()),
+      ).to.be.revertedWith(
+        'CreditScoreVerifiable: proof is required but it is not passed',
+      );
+    });
+
+    it(`reverts if the proof is not the caller's`, async () => {
+      await expect(
+        contract.proofRequiredDoSomething(getScoreProof(userCreditScore, tree)),
+      ).to.be.revertedWith(
+        'CreditScoreVerifiable: proof does not belong to the caller',
+      );
+    });
+
+    it('updates the credit score if the proof is passed', async () => {
+      let lastCreditScore = await creditScoreContract.getLastScore(
+        owner.address,
+      );
+      expect(lastCreditScore[0]).to.eq(0);
+
+      await contract.proofRequiredDoSomething(
+        getScoreProof(ownerCreditScore, tree),
+      );
+
+      lastCreditScore = await creditScoreContract.getLastScore(owner.address);
+      expect(lastCreditScore[0]).to.eq(ownerCreditScore.amount);
+    });
+
+    it('does not update the credit score if proof is not passed and not required', async () => {
+      let lastCreditScore = await creditScoreContract.getLastScore(
+        owner.address,
+      );
+      expect(lastCreditScore[0]).to.eq(0);
+
+      await contract.proofOptionalDoSomething(getEmptyScoreProof());
+
+      lastCreditScore = await creditScoreContract.getLastScore(owner.address);
+      expect(lastCreditScore[0]).to.eq(0);
+
+      await contract.proofOptionalDoSomething(
+        getScoreProof(ownerCreditScore, tree),
+      );
+
+      lastCreditScore = await creditScoreContract.getLastScore(owner.address);
+      expect(lastCreditScore[0]).to.eq(ownerCreditScore.amount);
+    });
+  });
+});
