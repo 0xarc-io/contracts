8823864e876aa3aca2cebbc5f07d1c4bc45c8efb
Gabriel T <g@arcx.money>
2021-07-09 00:58:36 -0400

    style: ignore empty block

--- contracts/test/MockStakingAccrualERC20.sol
+++ contracts/test/MockStakingAccrualERC20.sol
@@ -6,6 +6,4 @@ pragma experimental ABIEncoderV2;
 import {StakingAccrualERC20} from "../staking/StakingAccrualERC20.sol";
 import { MockTimestamp } from "./MockTimestamp.sol";
 
-contract MockStakingAccrualERC20 is StakingAccrualERC20, MockTimestamp {
-
-}
+contract MockStakingAccrualERC20 is StakingAccrualERC20, MockTimestamp {/* solium-disable-line no-empty-blocks */}
