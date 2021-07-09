b488b1104265f864219a22f153c2c583f658c928
Gabriel T <g@arcx.money>
2021-07-08 22:24:04 -0400

    test(stakingaccrualerc20.test.ts): implement tests

--- contracts/staking/StakingAccrualERC20.sol
+++ contracts/staking/StakingAccrualERC20.sol
@@ -8,6 +8,7 @@ import {Initializable} from "../lib/Initializable.sol";
 import {Address} from "../lib/Address.sol";
 
 import {BaseERC20} from "../token/BaseERC20.sol";
+import {IPermittableERC20} from "../token/IPermittableERC20.sol";
 
 /**
  * @notice An ERC20 that allows users to deposit a given token, where their
@@ -28,6 +29,13 @@ contract StakingAccrualERC20 is BaseERC20, Adminable, Initializable {
 
     uint8 private _decimals;
 
+    IPermittableERC20 public stakingToken;
+
+    /**
+     * @notice Cooldown duration to be elapsed for users to exit
+     */
+    uint256 public exitCooldownDuration;
+
     /* ========== Constructor (ignore) ========== */
 
     constructor ()
@@ -41,7 +49,8 @@ contract StakingAccrualERC20 is BaseERC20, Adminable, Initializable {
         string calldata __name,
         string calldata __symbol,
         uint8 __decimals,
-        address _stakingToken
+        address _stakingToken,
+        uint256 _exitCooldownDuration
     )
         external
         onlyAdmin
@@ -50,10 +59,15 @@ contract StakingAccrualERC20 is BaseERC20, Adminable, Initializable {
         _name = __name;
         _symbol = __symbol;
         _decimals = __decimals;
+        exitCooldownDuration = _exitCooldownDuration;
 
         require (
             _stakingToken.isContract(),
             "StakingAccrualERC20: staking token is not a contract"
         );
+
+        stakingToken = IPermittableERC20(_stakingToken);
     }
+
+    // TODO: add stakeWithPermit()
 }
