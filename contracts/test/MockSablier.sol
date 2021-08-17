76c777e9cdb5a3463ba187c3e864766b6b375cc4
Gabriel <gtupak@users.noreply.github.com>
2021-08-17 07:57:03 -0400

    Update contracts/test/MockSablier.sol
    Co-authored-by: Aleks Clapin-Pepin <aclapinpepin@gmail.com>

--- contracts/test/MockSablier.sol
+++ contracts/test/MockSablier.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
-import { Sablier } from "../global/Sablier.sol";
+import {Sablier} from "../global/Sablier.sol";
 import {MockTimestamp} from "./MockTimestamp.sol";
 
 contract MockSablier is Sablier, MockTimestamp {/* solium-disable-line no-empty-blocks */}
