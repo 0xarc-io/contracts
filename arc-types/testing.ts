19ed6853e429960dbcd6bc56ccaff703bbad2ed1
Gabriel T <g@arcx.money>
2021-08-26 20:23:07 -0400

    chore: remove more references

--- arc-types/testing.ts
+++ arc-types/testing.ts
@@ -2,14 +2,9 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-wit
 
 import { MockContract } from 'ethereum-waffle';
 import { AddressAccrual } from '@src/typings/AddressAccrual';
-import { MockOracle } from '@src/typings/MockOracle';
 
 import { TestToken } from '@src/typings/TestToken';
-import { SpritzTestArc } from '@src/SpritzTestArc';
-import { MozartTestArc } from '@src/MozartTestArc';
 import {
-  MockMozartCoreV2,
-  MockMozartSavingsV2,
   MockSapphireCoreV1,
   MockSapphireCreditScore,
   SapphireAssessor,
@@ -34,25 +29,10 @@ export interface TestingSigners {
 }
 
 export interface SDKs {
-  spritz?: SpritzTestArc;
-  mozart?: MozartTestArc;
   sapphire?: SapphireTestArc;
 }
 
 export interface Contracts {
-  spritz: {
-    coreV1: CoreV1;
-    coreV2: CoreV2;
-    coreV3: CoreV3;
-    coreV4: CoreV4;
-    state: StateV1;
-    oracle: MockOracle;
-  };
-  mozart: {
-    core: MockMozartCoreV2;
-    savings: MockMozartSavingsV2;
-    oracle: MockOracle;
-  };
   sapphire: {
     core: MockSapphireCoreV1;
     creditScore: MockSapphireCreditScore;
@@ -61,31 +41,15 @@ export interface Contracts {
     oracle: MockSapphireOracle;
   };
   synthetic: {
-    static: StaticSyntheticToken;
-    tokenV1: SyntheticTokenV1;
     tokenV2: SyntheticTokenV2;
   };
   staking: {
     addressAccrual: AddressAccrual;
-    adminRewards: AdminRewards;
-    rewardCampaign: RewardCampaign;
-    tokenStakingAccrual: TokenStakingAccrual;
   };
   collateral: TestToken;
 }
 
 export interface Stubs {
-  spritz: {
-    coreV1: MockContract;
-    coreV2: MockContract;
-    coreV3: MockContract;
-    coreV4: MockContract;
-    state: MockContract;
-  };
-  mozart: {
-    coreV1: MockContract;
-    savingsV1: MockContract;
-  };
   synthetic: {
     static: MockContract;
     tokenV1: MockContract;
