40b8e70d4a9bedfbde60456eda965b017599f001
Gabriel T <g@arcx.money>
2021-09-10 18:15:31 -0400

    feat(passportscoreverifiable.sol): verify score if's required, proof passed or score > 0

--- contracts/lib/PassportScoreVerifiable.sol
+++ contracts/lib/PassportScoreVerifiable.sol
@@ -36,14 +36,7 @@ contract PassportScoreVerifiable {
 
         bool isProofPassed = _scoreProof.merkleProof.length > 0;
 
-        if (_isScoreRequired || _scoreProof.score > 0) {
-            require(
-                isProofPassed,
-                "PassportScoreVerifiable: proof is required but it is not passed"
-            );
-        }
-
-        if (isProofPassed) {
+        if (_isScoreRequired || isProofPassed || _scoreProof.score > 0) {
             passportScoresContract.verify(_scoreProof);
         }
         _;
