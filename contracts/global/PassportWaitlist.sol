7fed6da53bb2eccad4e6e7c4195b1a9c072ea9cf
Gabrel T <g@arcx.money>
2021-10-01 13:24:28 -0400

    feat(passportwaitlist.test.ts): add tests

---
+++ contracts/global/PassportWaitlist.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+import {Ownable} from "../lib/Ownable.sol";
+
+contract PassportWaitlist is Ownable {
+
+}
