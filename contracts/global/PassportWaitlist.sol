3bd66abff5ddb5556bb3e8b8143c16db37875124
Gabrel T <g@arcx.money>
2021-10-01 13:58:41 -0400

    feat(passportwaitlist.sol): implement apply

--- contracts/global/PassportWaitlist.sol
+++ contracts/global/PassportWaitlist.sol
@@ -5,6 +5,7 @@ pragma experimental ABIEncoderV2;
 
 import {Ownable} from "../lib/Ownable.sol";
 import {Address} from "../lib/Address.sol";
+import {SafeERC20} from "../lib/SafeERC20.sol";
 import {IPermittableERC20} from "../token/IPermittableERC20.sol";
 
 contract PassportWaitlist is Ownable {
@@ -14,8 +15,15 @@ contract PassportWaitlist is Ownable {
     using Address for address;
     
     /* ========== Events ========== */
-    /* ========== Storage ========== */
 
+    event UserApplied(
+        address _user,
+        address _paymentToken,
+        uint256 _paymentAmount,
+        uint256 _timestamp
+    );
+
+    /* ========== Storage ========== */
 
     IPermittableERC20 public paymentToken;
     uint256           public paymentAmount;
@@ -36,8 +44,62 @@ contract PassportWaitlist is Ownable {
             "PassportWaitlist: payment amount must be greater than 0"
         );
 
+        require(
+            _paymentReceiver != address(0),
+            "PassportWaitlist: payment receiver cannot be address 0"
+        );
+
         paymentToken    = IPermittableERC20(_paymentToken);
         paymentAmount   = _paymentAmount;
         paymentReceiver = _paymentReceiver;
+    }
+
+    /* ========== Public Functions ========== */
+
+    function applyForPassport() public
+    {
+        SafeERC20.safeTransferFrom(
+            paymentToken, 
+            msg.sender, 
+            paymentReceiver, 
+            paymentAmount
+        );
+
+        emit UserApplied(
+            msg.sender,
+            address(paymentToken),
+            paymentAmount,
+            currentTimestamp()
+        );
+    }
+
+    function permitApplyForPassport (
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    )
+        external
+    {
+        paymentToken.permit(
+            msg.sender,
+            address(this),
+            paymentAmount,
+            _deadline,
+            _v,
+            _r,
+            _s
+        );
+        applyForPassport();
+    }
+
+    /* ========== Dev Functions ========== */
+
+    function currentTimestamp()
+        public
+        view
+        returns (uint256)
+    {
+        return block.timestamp;
     }
 }
