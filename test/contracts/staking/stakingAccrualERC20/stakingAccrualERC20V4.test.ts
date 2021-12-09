be4499cc2915cb1f80f5a37afba07a62744aab54
Gabrel T <g@arcx.money>
2021-12-08 19:11:44 -0500

    test(stakingaccrualerc20v4.test.ts): added init base tests

--- test/contracts/staking/stakingAccrualERC20/stakingAccrualERC20V4.test.ts
+++ test/contracts/staking/stakingAccrualERC20/stakingAccrualERC20V4.test.ts
@@ -6,6 +6,7 @@ import {
   DefaultPassportSkin,
   DefaultPassportSkinFactory,
   DefiPassport,
+  MockSablier,
   MockSablierFactory,
   MockSapphirePassportScores,
   MockStakingAccrualERC20V4,
@@ -38,6 +39,7 @@ describe('StakingAccrualERC20V4', () => {
   let stakingToken: TestToken;
   let tree: PassportScoreTree;
   let passportScores: MockSapphirePassportScores;
+  let sablierContract: MockSablier;
 
   let admin: SignerWithAddress;
   let user1: SignerWithAddress;
@@ -94,7 +96,7 @@ describe('StakingAccrualERC20V4', () => {
       admin.address,
     );
 
-    const sablierContract = await new MockSablierFactory(admin).deploy();
+    sablierContract = await new MockSablierFactory(admin).deploy();
 
     // Setup default skin for defi passport
     const defaultPassportSkinContract = await new DefaultPassportSkinFactory(
@@ -209,161 +211,288 @@ describe('StakingAccrualERC20V4', () => {
 
   addSnapshotBeforeRestoreAfterEach();
 
-  describe('Upgradability', () => {
-    it('ensures starcx balances are unchanged', async () => {
-      expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
-      expect(await contract.balanceOf(user2.address)).to.eq(STAKE_AMOUNT);
-    });
+  describe('Base functionality tests', () => {
+    describe('Admin functions', () => {
+      let uninitializedContract: MockStakingAccrualERC20V4;
 
-    it('ensures users can exit', async () => {
-      const remainingAmt = INITIAL_BALANCE.sub(STAKE_AMOUNT);
-      // We are adding an extra STAKE_AMOUNT on top of what's staked to simulate
-      // rewards flowing into the contract. For this reason, the users end up with an
-      // additional STAKE_AMOUNT/2 in their wallet after exit.
-      const expectedExitAmt = INITIAL_BALANCE.add(STAKE_AMOUNT.div(2));
+      before(async () => {
+        const uninitializedContractImpl = await new MockStakingAccrualERC20V4Factory(
+          admin,
+        ).deploy();
+        const proxy = await new ArcProxyFactory(admin).deploy(
+          uninitializedContractImpl.address,
+          admin.address,
+          [],
+        );
+        uninitializedContract = MockStakingAccrualERC20V4Factory.connect(
+          proxy.address,
+          admin,
+        );
+      });
 
-      expect(await stakingToken.balanceOf(user1.address)).to.eq(remainingAmt);
-      expect(await stakingToken.balanceOf(user2.address)).to.eq(remainingAmt);
+      describe('#init', () => {
+        it('reverts if called by non-admin', async () => {
+          await expect(
+            uninitializedContract
+              .connect(user1)
+              .init(
+                'asdf',
+                'asdf',
+                18,
+                stakingToken.address,
+                COOLDOWN_DURATION,
+                sablierContract.address,
+              ),
+          ).to.be.revertedWith('Adminable: caller is not admin');
+        });
 
-      await contract.connect(user1).startExitCooldown();
-      await contract.setCurrentTimestamp(COOLDOWN_DURATION + 1);
-      await contract.connect(user1).exit();
-      expect(await stakingToken.balanceOf(user1.address)).to.eq(
-        expectedExitAmt,
-      );
+        it('reverts if called twice', async () => {
+          await uninitializedContract.init(
+            'test',
+            'test',
+            18,
+            stakingToken.address,
+            COOLDOWN_DURATION,
+            sablierContract.address,
+          );
 
-      await contract.connect(user2).startExitCooldown();
-      await contract.setCurrentTimestamp(COOLDOWN_DURATION * 2 + 1);
-      await contract.connect(user2).exit();
-      expect(await stakingToken.balanceOf(user2.address)).to.eq(
-        expectedExitAmt,
-      );
+          await expect(
+            uninitializedContract.init(
+              'asdf',
+              'asdf',
+              18,
+              stakingToken.address,
+              COOLDOWN_DURATION,
+              sablierContract.address,
+            ),
+          ).to.be.revertedWith(
+            'Initializable: contract is already initialized',
+          );
+        });
+
+        it('reverts if the staking token is address 0', async () => {
+          await expect(
+            uninitializedContract.init(
+              'asdf',
+              'qwer',
+              18,
+              constants.AddressZero,
+              COOLDOWN_DURATION,
+              sablierContract.address,
+            ),
+          ).to.be.revertedWith(
+            'StakingAccrualERC20V4: staking token is not a contract',
+          );
+          await expect(
+            uninitializedContract.init(
+              'asdf',
+              'qwer',
+              18,
+              user1.address,
+              COOLDOWN_DURATION,
+              sablierContract.address,
+            ),
+          ).to.be.revertedWith(
+            'StakingAccrualERC20V4: staking token is not a contract',
+          );
+        });
+
+        it('sets the staking token, the staking cooldown and the sablier contract', async () => {
+          await uninitializedContract.init(
+            'test',
+            'test',
+            18,
+            stakingToken.address,
+            COOLDOWN_DURATION,
+            sablierContract.address,
+          );
+
+          expect(await uninitializedContract.stakingToken()).to.eq(
+            stakingToken.address,
+          );
+          expect(await uninitializedContract.exitCooldownDuration()).to.eq(
+            COOLDOWN_DURATION,
+          );
+          expect(await uninitializedContract.sablierContract()).to.eq(
+            sablierContract.address,
+          );
+
+          expect(await uninitializedContract.decimals()).to.eq(18);
+          expect(await uninitializedContract.DOMAIN_SEPARATOR()).to.not.eq(
+            '0x0000000000000000000000000000000000000000000000000000000000000000',
+          );
+        });
+      });
     });
   });
 
-  describe('#stake', () => {
-    beforeEach(async () => {
-      expect(await contract.passportScoresContract()).to.eq(
-        passportScores.address,
-      );
+  describe('Upgrade specific tests', () => {
+    describe('Upgradability', () => {
+      it('ensures starcx balances are unchanged', async () => {
+        expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
+        expect(await contract.balanceOf(user2.address)).to.eq(STAKE_AMOUNT);
+      });
 
-      await contract.setProofProtocol(
-        utils.formatBytes32String(DEFAULT_PROOF_PROTOCOL),
-      );
-      await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD);
+      it('ensures users can exit', async () => {
+        const remainingAmt = INITIAL_BALANCE.sub(STAKE_AMOUNT);
+        // We are adding an extra STAKE_AMOUNT on top of what's staked to simulate
+        // rewards flowing into the contract. For this reason, the users end up with an
+        // additional STAKE_AMOUNT/2 in their wallet after exit.
+        const expectedExitAmt = INITIAL_BALANCE.add(STAKE_AMOUNT.div(2));
+
+        expect(await stakingToken.balanceOf(user1.address)).to.eq(remainingAmt);
+        expect(await stakingToken.balanceOf(user2.address)).to.eq(remainingAmt);
+
+        await contract.connect(user1).startExitCooldown();
+        await contract.setCurrentTimestamp(COOLDOWN_DURATION + 1);
+        await contract.connect(user1).exit();
+        expect(await stakingToken.balanceOf(user1.address)).to.eq(
+          expectedExitAmt,
+        );
+
+        await contract.connect(user2).startExitCooldown();
+        await contract.setCurrentTimestamp(COOLDOWN_DURATION * 2 + 1);
+        await contract.connect(user2).exit();
+        expect(await stakingToken.balanceOf(user2.address)).to.eq(
+          expectedExitAmt,
+        );
+      });
     });
 
-    it('reverts if proof is set and no proof is provided', async () => {
-      expect(await contract.getProofProtocol()).to.eq(DEFAULT_PROOF_PROTOCOL);
-      expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
+    describe('#stake', () => {
+      beforeEach(async () => {
+        expect(await contract.passportScoresContract()).to.eq(
+          passportScores.address,
+        );
 
-      await expect(
-        contract.stake(
-          STAKE_AMOUNT,
-          getEmptyScoreProof(
-            user1.address,
-            utils.formatBytes32String(DEFAULT_PROOF_PROTOCOL),
+        await contract.setProofProtocol(
+          utils.formatBytes32String(DEFAULT_PROOF_PROTOCOL),
+        );
+        await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD);
+      });
+
+      it('reverts if proof is set and no proof is provided', async () => {
+        expect(await contract.getProofProtocol()).to.eq(DEFAULT_PROOF_PROTOCOL);
+        expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
+
+        await expect(
+          contract.stake(
+            STAKE_AMOUNT,
+            getEmptyScoreProof(
+              user1.address,
+              utils.formatBytes32String(DEFAULT_PROOF_PROTOCOL),
+            ),
           ),
-        ),
-      ).to.be.revertedWith(
-        'PassportScoreVerifiable: proof does not belong to the caller',
-      );
+        ).to.be.revertedWith(
+          'PassportScoreVerifiable: proof does not belong to the caller',
+        );
+      });
+
+      it('reverts if proof is set and the wrong proof is passed', async () => {
+        await expect(
+          contract.connect(user1).stake(STAKE_AMOUNT, scoreProof2),
+        ).to.be.revertedWith(
+          'PassportScoreVerifiable: proof does not belong to the caller',
+        );
+      });
+
+      it('reverts if proof is set and the score is smaller than the threshold', async () => {
+        await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD + 1);
+        await expect(
+          contract.connect(user1).stake(STAKE_AMOUNT, scoreProof1),
+        ).to.be.revertedWith('StakingAccrualERC20V4: score is below threshold');
+      });
+
+      it('stakes if proof is set and score is greater than or equal to the threshold', async () => {
+        expect(await contract.getProofProtocol()).to.eq(DEFAULT_PROOF_PROTOCOL);
+        expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
+
+        expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
+
+        await contract.connect(user1).stake(STAKE_AMOUNT, scoreProof1);
+
+        const totalShares = await contract.totalSupply();
+        const totalStakingToken = await stakingToken.balanceOf(
+          contract.address,
+        );
+        expect(await contract.balanceOf(user1.address)).to.eq(
+          STAKE_AMOUNT.add(
+            STAKE_AMOUNT.mul(totalShares).div(totalStakingToken),
+          ),
+        );
+      });
+
+      it('stakes if no proof is passed', async () => {
+        await contract.setProofProtocol(constants.HashZero);
+
+        expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
+
+        await contract
+          .connect(user1)
+          .stake(STAKE_AMOUNT, getEmptyScoreProof(user1.address));
+
+        const totalShares = await contract.totalSupply();
+        const totalStakingToken = await stakingToken.balanceOf(
+          contract.address,
+        );
+        expect(await contract.balanceOf(user1.address)).to.eq(
+          STAKE_AMOUNT.add(
+            STAKE_AMOUNT.mul(totalShares).div(totalStakingToken),
+          ),
+        );
+      });
     });
 
-    it('reverts if proof is set and the wrong proof is passed', async () => {
-      await expect(
-        contract.connect(user1).stake(STAKE_AMOUNT, scoreProof2),
-      ).to.be.revertedWith(
-        'PassportScoreVerifiable: proof does not belong to the caller',
-      );
+    describe('#setProofProtocol', () => {
+      it('reverts if called by non-admin', async () => {
+        await expect(
+          contract
+            .connect(user1)
+            .setProofProtocol(utils.formatBytes32String('')),
+        ).to.be.revertedWith('Adminable: caller is not admin');
+      });
+
+      it('sets the proof protocol', async () => {
+        expect(await contract.getProofProtocol()).to.eq('');
+
+        await contract.setProofProtocol(utils.formatBytes32String('test'));
+
+        expect(await contract.getProofProtocol()).to.eq('test');
+      });
     });
 
-    it('reverts if proof is set and the score is smaller than the threshold', async () => {
-      await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD + 1);
-      await expect(
-        contract.connect(user1).stake(STAKE_AMOUNT, scoreProof1),
-      ).to.be.revertedWith('StakingAccrualERC20V4: score is below threshold');
+    describe('#setScoreThreshold', () => {
+      it('reverts if called by non-admin', async () => {
+        await expect(
+          contract.connect(user1).setScoreThreshold(21),
+        ).to.be.revertedWith('Adminable: caller is not admin');
+      });
+
+      it('sets the score threshold', async () => {
+        expect(await contract.scoreThreshold()).to.eq(0);
+
+        await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD);
+
+        expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
+      });
     });
 
-    it('stakes if proof is set and score is greater than or equal to the threshold', async () => {
-      expect(await contract.getProofProtocol()).to.eq(DEFAULT_PROOF_PROTOCOL);
-      expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
+    describe('#setPassportScoresContract', () => {
+      it('reverts if called by non-admin', async () => {
+        await expect(
+          contract.connect(user1).setPassportScoresContract(contract.address),
+        ).to.be.revertedWith('Adminable: caller is not admin');
+      });
 
-      expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
+      it('sets the passport scores contract', async () => {
+        expect(await contract.passportScoresContract()).to.eq(
+          passportScores.address,
+        );
 
-      await contract.connect(user1).stake(STAKE_AMOUNT, scoreProof1);
+        await contract.setPassportScoresContract(contract.address);
 
-      const totalShares = await contract.totalSupply();
-      const totalStakingToken = await stakingToken.balanceOf(contract.address);
-      expect(await contract.balanceOf(user1.address)).to.eq(
-        STAKE_AMOUNT.add(STAKE_AMOUNT.mul(totalShares).div(totalStakingToken)),
-      );
-    });
-
-    it('stakes if no proof is passed', async () => {
-      await contract.setProofProtocol(constants.HashZero);
-
-      expect(await contract.balanceOf(user1.address)).to.eq(STAKE_AMOUNT);
-
-      await contract
-        .connect(user1)
-        .stake(STAKE_AMOUNT, getEmptyScoreProof(user1.address));
-
-      const totalShares = await contract.totalSupply();
-      const totalStakingToken = await stakingToken.balanceOf(contract.address);
-      expect(await contract.balanceOf(user1.address)).to.eq(
-        STAKE_AMOUNT.add(STAKE_AMOUNT.mul(totalShares).div(totalStakingToken)),
-      );
-    });
-  });
-
-  describe('#setProofProtocol', () => {
-    it('reverts if called by non-admin', async () => {
-      await expect(
-        contract.connect(user1).setProofProtocol(utils.formatBytes32String('')),
-      ).to.be.revertedWith('Adminable: caller is not admin');
-    });
-
-    it('sets the proof protocol', async () => {
-      expect(await contract.getProofProtocol()).to.eq('');
-
-      await contract.setProofProtocol(utils.formatBytes32String('test'));
-
-      expect(await contract.getProofProtocol()).to.eq('test');
-    });
-  });
-
-  describe('#setScoreThreshold', () => {
-    it('reverts if called by non-admin', async () => {
-      await expect(
-        contract.connect(user1).setScoreThreshold(21),
-      ).to.be.revertedWith('Adminable: caller is not admin');
-    });
-
-    it('sets the score threshold', async () => {
-      expect(await contract.scoreThreshold()).to.eq(0);
-
-      await contract.setScoreThreshold(DEFAULT_SCORE_THRESHOLD);
-
-      expect(await contract.scoreThreshold()).to.eq(DEFAULT_SCORE_THRESHOLD);
-    });
-  });
-
-  describe('#setPassportScoresContract', () => {
-    it('reverts if called by non-admin', async () => {
-      await expect(
-        contract.connect(user1).setPassportScoresContract(contract.address),
-      ).to.be.revertedWith('Adminable: caller is not admin');
-    });
-
-    it('sets the passport scores contract', async () => {
-      expect(await contract.passportScoresContract()).to.eq(
-        passportScores.address,
-      );
-
-      await contract.setPassportScoresContract(contract.address);
-
-      expect(await contract.passportScoresContract()).to.eq(contract.address);
+        expect(await contract.passportScoresContract()).to.eq(contract.address);
+      });
     });
   });
 });
