b72a48e5c43f022db11e6960007a02d7998c4161
Gabrel T <g@arcx.money>
2021-12-14 11:51:51 -0500

    refactor: use v5 by default in the deployment script of starcx and rename function

--- test/contracts/staking/stakingAccrualERC20/checkStakedBalance.ts
+++ test/contracts/staking/stakingAccrualERC20/checkStakedBalance.ts
@@ -6,7 +6,7 @@ import {
 } from '@src/typings';
 import { expect } from 'chai';
 
-async function checkUserBalance(
+async function checkStakedBalance(
   starcx: MockStakingAccrualERC20 | MockStakingAccrualERC20V5,
   user: SignerWithAddress,
 ) {
@@ -18,4 +18,4 @@ async function checkUserBalance(
   expect(await starcx.toStakedToken(arcAmount)).eq(stArcxBalance);
 }
 
-export default checkUserBalance;
+export default checkStakedBalance;
