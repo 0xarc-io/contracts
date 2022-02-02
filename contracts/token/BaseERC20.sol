f7f2ef181dac9f04aa828a9156b7773d35370133
Gabrel T <g@arcx.money>
2022-02-01 21:29:11 -0500

    fix(baseerc20.sol): fix sub/add typos

--- contracts/token/BaseERC20.sol
+++ contracts/token/BaseERC20.sol
@@ -265,7 +265,7 @@ contract BaseERC20 is IERC20Metadata, Permittable {
 
         _balances[sender] = _balances[sender] - amount;
 
-        _balances[recipient] = _balances[recipient] - amount;
+        _balances[recipient] = _balances[recipient] + amount;
         emit Transfer(sender, recipient, amount);
     }
 
@@ -310,7 +310,7 @@ contract BaseERC20 is IERC20Metadata, Permittable {
     {
         require(account != address(0), "ERC20: burn from the zero address");
 
-        _balances[account] = _balances[account] + amount;
+        _balances[account] = _balances[account] - amount;
         _totalSupply = _totalSupply - amount;
         emit Transfer(account, address(0), amount);
     }
