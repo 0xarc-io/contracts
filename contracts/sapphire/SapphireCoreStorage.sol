c09732b948460c61370b65ddb7df3e1112d16805
Gabrel T <g@arcx.money>
2022-02-09 21:37:42 -0500

    feat(sapphirecorestorage.sol): add borrow pool

--- contracts/sapphire/SapphireCoreStorage.sol
+++ contracts/sapphire/SapphireCoreStorage.sol
@@ -23,89 +23,94 @@ contract SapphireCoreStorageV1 {
     bool public paused;
 
     /**
-     * @dev The details about a vault, identified by the address of the owner
+     * @notice The details about a vault, identified by the address of the owner
      */
     mapping (address => SapphireTypes.Vault) public vaults;
 
     /**
-    * @dev The high/default collateral ratio for an untrusted borrower.
+    * @notice The high/default collateral ratio for an untrusted borrower.
     */
     uint256 public highCollateralRatio;
 
     /**
-    * @dev The lowest collateral ratio for an untrusted borrower.
+    * @notice The lowest collateral ratio for an untrusted borrower.
     */
     uint256 public lowCollateralRatio;
 
     /**
-     * @dev How much should the liquidation penalty be, expressed as a percentage
+     * @notice How much should the liquidation penalty be, expressed as a percentage
      *      with 18 decimals
      */
     uint256 public liquidationUserRatio;
 
     /**
-     * @dev How much of the profit acquired from a liquidation should ARC receive
+     * @notice How much of the profit acquired from a liquidation should ARC receive
      */
     uint256 public liquidationArcRatio;
 
     /**
-    * @dev The assessor that will determine the collateral-ratio.
+    * @notice The assessor that will determine the collateral-ratio.
     */
     ISapphireAssessor public assessor;
 
     /**
-    * @dev The address which collects fees when liquidations occur.
+    * @notice The address which collects fees when liquidations occur.
     */
     address public feeCollector;
 
     /**
-     * @dev The instance of the oracle that reports prices for the collateral
+     * @notice The instance of the oracle that reports prices for the collateral
      */
     ISapphireOracle public oracle;
 
     /**
-     * @dev If a erc20 asset is used that has less than 18 decimal places
+     * @notice If a erc20 asset is used that has less than 18 decimal places
      *      a precision scalar is required to calculate the correct values.
      */
     mapping(address => uint256) public precisionScalars;
 
     /**
-     * @dev The actual address of the collateral used for this core system.
+     * @notice The actual address of the collateral used for this core system.
      */
     address public collateralAsset;
 
     /**
-     * @dev The address of the synthetic token where this core is approved to mint from
+     * @notice The address of the synthetic token where this core is approved to mint from
      */
     address public syntheticAsset;
 
     /**
-    * @dev The actual amount of collateral provided to the protocol.
+     * @notice The address of the SapphirePool - the contract where the borrowed tokens come from
+     */
+    address public borrowPool;
+
+    /**
+    * @notice The actual amount of collateral provided to the protocol.
     *      This amount will be multiplied by the precision scalar if the token
     *      has less than 18 decimals precision.
     */
     uint256 public totalCollateral;
 
     /**
-     * @dev An account of the total amount being borrowed by all depositors. This includes
+     * @notice An account of the total amount being borrowed by all depositors. This includes
      *      the amount of interest accrued.
      */
     uint256 public totalBorrowed;
 
     /**
-     * @dev The accumulated borrow index. Each time a borrows, their borrow amount is expressed
+     * @notice The accumulated borrow index. Each time a borrows, their borrow amount is expressed
      *      in relation to the borrow index.
      */
     uint256 public borrowIndex;
 
     /**
-     * @dev The last time the updateIndex() function was called. This helps to determine how much
+     * @notice The last time the updateIndex() function was called. This helps to determine how much
      *      interest has accrued in the contract since a user interacted with the protocol.
      */
     uint256 public indexLastUpdate;
 
     /**
-     * @dev The interest rate charged to borrowers. Expressed as the interest rate per second and 18 d.p
+     * @notice The interest rate charged to borrowers. Expressed as the interest rate per second and 18 d.p
      */
     uint256 public interestRate;
 
@@ -115,18 +120,18 @@ contract SapphireCoreStorageV1 {
     address public interestSetter;
 
     /**
-     * @dev The address that can call `setPause()`
+     * @notice The address that can call `setPause()`
      */
     address public pauseOperator;
 
     /**
-     * @dev The minimum amount which has to be borrowed by a vault. This includes
-     *      the amount of interest accrued.
+     * @notice The minimum amount which has to be borrowed by a vault. This includes
+     *         the amount of interest accrued.
      */
     uint256 public vaultBorrowMinimum;
 
     /**
-     * @dev The maximum amount which has to be borrowed by a vault. This includes
+     * @notice The maximum amount which has to be borrowed by a vault. This includes
      *      the amount of interest accrued.
      */
     uint256 public vaultBorrowMaximum;
