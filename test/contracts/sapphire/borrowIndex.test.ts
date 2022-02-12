6d9da603e954db6ef596fcf315d416e7a426f4b1
Gabrel T <g@arcx.money>
2022-02-12 17:50:57 -0500

    refactor(setup.ts): call setupPool() only in setupSapphrie()

--- test/contracts/sapphire/borrowIndex.test.ts
+++ test/contracts/sapphire/borrowIndex.test.ts
@@ -24,7 +24,7 @@ import { expect } from 'chai';
 import { BigNumber, BigNumberish, utils } from 'ethers';
 import { generateContext, ITestContext } from '../context';
 import { sapphireFixture } from '../fixtures';
-import { setupPool, setupSapphire } from '../setup';
+import { setupSapphire } from '../setup';
 import { roundUpDiv, roundUpMul } from '@test/helpers/roundUpOperations';
 import { TestToken } from '@src/typings';
 
