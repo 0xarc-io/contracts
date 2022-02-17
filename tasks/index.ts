65125f3f8e3a42bf1acf5d54178c8e612bf43ee0
Gabrel T <g@arcx.money>
2022-02-17 18:29:35 -0500

    refactor: uncomment code

--- tasks/index.ts
+++ tasks/index.ts
@@ -14,7 +14,7 @@ export * from './distributor-tasks';
 import { task } from 'hardhat/config';
 
 task('deploy-setup', 'Deploy all the smart contracts locally').setAction(
-  async (taskArgs, hre) => {
+  async (_, hre) => {
     await hre.run('deploy-global');
   },
 );
