45061376b96fa753aa314cc74b34033093b0b894
Gabrel T <g@arcx.money>
2022-04-28 16:31:06 -0400

    chore(sapphire-tasks.ts): add repair task

--- tasks/sapphire-tasks.ts
+++ tasks/sapphire-tasks.ts
@@ -32,6 +32,9 @@ import {
 } from '../deployments/types';
 import { TransactionRequest } from '@ethersproject/providers';
 import prompt from 'prompt';
+import { assert } from 'console';
+import { roundUpDiv } from '@test/helpers/roundUpOperations';
+import { BASE } from '@src/constants';
 
 task(
   'deploy-passport-scores',
@@ -480,6 +483,52 @@ task('deploy-borrow-pool')
       symbol,
     );
   });
+
+task('repair-withdraw-bug').setAction(async (taskArgs, hre) => {
+  const { signer, networkConfig } = await loadDetails(hre);
+
+  assert(signer.address === '0x9c767178528c8a205DF63305ebdA4BB6B147889b');
+
+  // Deploy new contract
+  const upgradeAddress = await deployContract(
+    {
+      name: 'SapphirePool',
+      source: 'SapphirePool',
+      data: new SapphirePoolFactory(signer).getDeployTransaction(),
+      version: 2,
+      type: DeploymentType.borrowing,
+    },
+    networkConfig,
+  );
+
+  // Upgrade contract
+  const poolProxyAddress = '0x59b8a21A0B0cE87E308082Af6fFC4205b5dC932C';
+  const poolProxy = await ArcProxyFactory.connect(poolProxyAddress, signer);
+  const upgradeTx = await poolProxy.upgradeTo(upgradeAddress);
+  await upgradeTx.wait();
+
+  // Deployer withdraws exactly the deposit utilization to set utilization to 0
+  const pool = await SapphirePoolFactory.connect(poolProxyAddress, signer);
+  const usdcAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+  const { amountUsed: assetUtilization } = await pool.assetDepositUtilization(
+    usdcAddress,
+  );
+  const supply = await pool.totalSupply();
+  const poolValue = await pool.getPoolValue();
+  const usdcScalar = 10 ** 12;
+  const depositUtilizationLpEquivalent = roundUpDiv(
+    assetUtilization.mul(usdcScalar).mul(supply).div(BASE),
+    poolValue,
+  );
+  await pool.withdraw(depositUtilizationLpEquivalent, usdcAddress);
+
+  // Set Deployer's utilization to 0
+  await pool.resetBorrowUtilization(signer.address);
+
+  // The utilization should be 0, and user's deposit should be 0
+  assert((await pool.assetDepositUtilization(usdcAddress)).amountUsed.eq(0));
+  assert((await pool.deposits(signer.address)).eq(0));
+});
 
 function _deployTestCollateral(
   networkConfig: NetworkParams,
