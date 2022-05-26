f68cda4876aa2877e581c9119f892938fa7f936c
Gabrel T <g@arcx.money>
2022-05-26 14:27:27 -0400

    refactor(utility-tasks.ts): extract checkLiquidatable to external file

--- tasks/utility-tasks.ts
+++ tasks/utility-tasks.ts
@@ -6,10 +6,21 @@ import {
   loadDetails,
   pruneDeployments,
 } from '../deployments/src';
-import { utils } from 'ethers';
+import { BigNumber, utils } from 'ethers';
 import { task } from 'hardhat/config';
-import { TestTokenFactory } from '@src/typings';
 import { DeploymentType } from '../deployments/types';
+import {
+  BaseERC20Factory,
+  SapphireCoreV1,
+  SapphireCoreV1Factory,
+  TestTokenFactory,
+} from '@src/typings';
+import { Filter, Log } from '@ethersproject/abstract-provider';
+import { BASE } from '@src/constants';
+import { PassportScoreProof } from '@arc-types/sapphireCore';
+import { approve } from '@src/utils';
+import { SapphireArc } from '@src/SapphireArc';
+import { checkLiquidatable } from './utils/checkLiquidatable';
 
 task('mint-tokens')
   .addParam('token', 'The address of the token to mint from')
@@ -95,3 +106,100 @@ task('deploy-test-erc20', 'Deploys an ERC20 test token with 18 decimals')
       constructorArguments: [name, symbol, 18],
     });
   });
+
+task('liquidate-borrowers').setAction(async (taskArgs, hre) => {
+  const { signer } = await loadDetails(hre);
+  const arc = SapphireArc.new(signer);
+  const coreProxyAddress = '0x05efe26f4a75EA4d183e8a7922494d60adfB27b3';
+  await arc.addCores({ sapphireCore: coreProxyAddress });
+
+  const core = SapphireCoreV1Factory.connect(coreProxyAddress, signer);
+
+  const contractCreationBlock = 26815547;
+
+  const borrowLogFilter: Filter = {
+    address: coreProxyAddress,
+    topics: core.filters.Borrowed(null, null, null, null, null, null).topics,
+    toBlock: 'latest',
+    fromBlock: contractCreationBlock,
+  };
+  const logs = await signer.provider.getLogs(borrowLogFilter);
+
+  const borrowers: string[] = getBorrowers(logs);
+
+  for (const addr of borrowers) {
+    const { assessedCRatio, isLiquidatable, proof } = await checkLiquidatable(
+      addr,
+      arc,
+    );
+
+    if (isLiquidatable) {
+      console.log(
+        `>>> User ${addr} can be liquidated! Their assessed c-ratio is ${utils.formatEther(
+          assessedCRatio,
+        )}\nStarting liquidation...`,
+      );
+
+      await liquidate(addr, core, proof);
+
+      console.log(green('>>> Liquidation complete!'));
+    }
+  }
+});
+
+function getBorrowers(logs: Log[]): string[] {
+  const borrowersFromAllTxs = logs.map((log) =>
+    utils.hexStripZeros(log.topics[1]),
+  );
+  const uniqueBorrowers = borrowersFromAllTxs.filter(
+    (addr, index, self) => self.indexOf(addr) === index,
+  );
+
+  return uniqueBorrowers;
+}
+
+async function liquidate(
+  addr: string,
+  core: SapphireCoreV1,
+  proof: PassportScoreProof,
+) {
+  const usdcAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+  const usdc = BaseERC20Factory.connect(usdcAddress, core.signer);
+  const usdcScalar = BigNumber.from(10 ** (18 - (await usdc.decimals())));
+  const borrowIndex = await core.currentBorrowIndex();
+
+  const preLiquidationVault = await core.vaults(addr);
+  const denormalizedBorrowAmt = preLiquidationVault.normalizedBorrowedAmount
+    .mul(borrowIndex)
+    .div(BASE);
+  console.log(`Before liquidation:`);
+  console.log(
+    `\t Collateral: ${utils.formatEther(
+      preLiquidationVault.collateralAmount,
+    )} ETH`,
+  );
+  console.log(`\t Debt: $${utils.formatEther(denormalizedBorrowAmt)}`);
+
+  await approve(
+    denormalizedBorrowAmt.add(BASE).div(usdcScalar), // +1 $ to account for ongoing interest
+    usdc.address,
+    core.address,
+    core.signer,
+  );
+  const tx = await core.liquidate(addr, usdcAddress, [proof]);
+  const receipt = await tx.wait();
+  console.log(green(`>>> Liquidation transaction: ${receipt.transactionHash}`));
+
+  const postLiquidationVault = await core.vaults(addr);
+  console.log(`After liquidation:`);
+  console.log(
+    `\t Collateral: ${utils.formatEther(
+      postLiquidationVault.collateralAmount,
+    )} ETH`,
+  );
+  console.log(
+    `\t Debt: $${utils.formatEther(
+      postLiquidationVault.normalizedBorrowedAmount.mul(borrowIndex),
+    )}`,
+  );
+}
---
+++ tasks/utils/checkLiquidatable.ts
@@ -0,0 +1,85 @@
+import { PassportScoreProof } from '@arc-types/sapphireCore';
+import { SapphireArc } from '@src/SapphireArc';
+import axios from 'axios';
+import { BigNumber, utils } from 'ethers';
+import { BASE } from '../../src';
+
+async function getUserCreditScoreProof(addr: string) {
+  const res = await axios.get(`https://api.arcx.money/v1/scores`, {
+    params: {
+      account: addr,
+      protocol: 'arcx.credit',
+      format: 'proof',
+    },
+  });
+
+  return res.data.data[0] as PassportScoreProof;
+}
+
+async function checkLiquidatable(
+  addr: string,
+  arc: SapphireArc,
+  proof?: PassportScoreProof,
+) {
+  const { core, assessor, oracle } = arc.getCoreContracts(
+    arc.getCoreNames()[0],
+  );
+
+  const { price } = await oracle.fetchCurrentPrice();
+  const maxScore = await assessor.maxScore();
+  const maxCRatio = await core.highCollateralRatio();
+  const minCRatio = await core.lowCollateralRatio();
+  const boundsDifference = maxCRatio.sub(minCRatio);
+  const creditScoreProof = proof ?? (await getUserCreditScoreProof(addr));
+
+  const creditScore = BigNumber.from(creditScoreProof.score);
+  const assessedCRatio = maxCRatio.sub(
+    creditScore.mul(boundsDifference).div(maxScore),
+  );
+
+  const vault = await core.vaults(addr);
+  if (vault.normalizedBorrowedAmount.isZero()) {
+    return {
+      assessedCRatio: null,
+      isLiquidatable: false,
+    };
+  }
+
+  const denormalizedBorrowAmt = vault.normalizedBorrowedAmount
+    .mul(await core.currentBorrowIndex())
+    .div(BASE);
+  const collateralValue = vault.collateralAmount.mul(price).div(BASE);
+  const currentCRatio = collateralValue.mul(BASE).div(denormalizedBorrowAmt);
+
+  const isCollateralized = currentCRatio.gt(assessedCRatio);
+
+  console.log(
+    `user ${addr} has an assessed c-ratio of ${utils.formatEther(
+      assessedCRatio,
+    )}. Current c-ratio: ${utils.formatEther(
+      currentCRatio,
+    )}. There is a gap of ${utils.formatEther(
+      isCollateralized
+        ? currentCRatio.sub(assessedCRatio)
+        : assessedCRatio.sub(currentCRatio),
+    )}`,
+  );
+
+  if (!isCollateralized) {
+    console.log(`>>> vault ${addr} is subject to liquidation!`);
+    console.log(
+      `\t Collateral value: $${utils.formatEther(
+        collateralValue,
+      )} (${utils.formatEther(vault.collateralAmount)} ETH)`,
+    );
+    console.log(`\t Debt: $${utils.formatEther(denormalizedBorrowAmt)}`);
+  }
+
+  return {
+    assessedCRatio,
+    isLiquidatable: !isCollateralized,
+    proof: creditScoreProof,
+  };
+}
+
+export { checkLiquidatable };
